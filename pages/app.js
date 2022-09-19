75253f93b14f78c3cc29b9b8eedd5d6188c6b9c7
Nikolas Escobal <niko.escobal@gmail.com>
2022-09-20 02:23:42 +0800

    Add breakpoints partial, flesh out questions component with styles

--- pages/app.js
+++ pages/app.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import React from 'react'
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
 import Questions from '../components/App/Questions/Questions';
-import GenericHeader from '../components/Generic/Header/GenericHeader'
-import Character from '../public/nft/character1.png';
+import GenericHeader from '../components/Generic/Header/GenericHeader';
+import Character from '../public/nft/char-with-iphone.png';
 
 function app() {
   return (
@@ -13,10 +13,14 @@ function app() {
         <meta name="description" content="Legacy Network Application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <GenericHeader rightObj={<Image src={Character} alt="character" />} title="The Legacy Network App" subtitle={"The future of personal development"} />
+      <GenericHeader
+        rightObj={<Image src={Character} alt="character" />}
+        title="LGCN TOKEN"
+        subtitle={'The future of personal development'}
+      />
       <Questions />
     </div>
-  )
+  );
 }
 
-export default app
+export default app;
