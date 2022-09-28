2d3712a9f689ae604b3a111f43ebda3e04381f8f
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-09-28 09:04:14 +0200

    added connectors to card section and replaced icons

--- components/Home/CardSection/CardSection.jsx
+++ components/Home/CardSection/CardSection.jsx
@@ -1,8 +1,16 @@
 import styles from './cardsection.module.scss';
 import MovingIcon from '@mui/icons-material/Moving';
+import Icon1 from '../../../public/icons/card-icon1.svg';
+import Icon2 from '../../../public/icons/card-icon2.svg';
+import Icon3 from '../../../public/icons/card-icon3.svg';
+import Icon4 from '../../../public/icons/card-icon4.svg';
 import Image from 'next/image';
 import Chests from '../../../public/backgrounds/chests.png';
 import useStore from '../../../store/store';
+import Connector1 from '../../../public/illustrations/connector1.svg';
+import Connector2 from '../../../public/illustrations/connector2.svg';
+import Connector3 from '../../../public/illustrations/connector3.svg';
+import Link from 'next/link';
 
 function CardSection() {
   const { isDarkMode } = useStore();
@@ -21,9 +29,14 @@ function CardSection() {
         //   backgroundSize: 'cover',
         // }}
       >
+        {/* <div className={`${styles['connectors']}`}> */}
+          <img src={Connector1.src} alt="" className={`${styles['connector']}`} />
+          <img src={Connector2.src} alt="" className={`${styles['connector']}`} />
+          <img src={Connector3.src} alt="" className={`${styles['connector']}`} />
+        {/* </div> */}
         <div className={`${styles['card']}`}>
-          <div>
-            <MovingIcon />
+          <div className={`${styles['icon-wrapper']}`}>
+            <Image src={Icon1} alt="icon" />
           </div>
           <div>
             <h4>Play through content from lessons</h4>
@@ -34,8 +47,8 @@ function CardSection() {
           </div>
         </div>
         <div className={`${styles['card']}`}>
-          <div>
-            <MovingIcon />
+          <div className={`${styles['icon-wrapper']}`}>
+            <Image src={Icon2} alt="icon" />
           </div>
           <div>
             <h4>Complete the quest and level up</h4>
@@ -43,8 +56,8 @@ function CardSection() {
           </div>
         </div>
         <div className={`${styles['card']}`}>
-          <div>
-            <MovingIcon />
+          <div className={`${styles['icon-wrapper']}`}>
+            <Image src={Icon3} alt="icon" />
           </div>
           <div>
             <h4>Solve quest and test your knowledge</h4>
@@ -55,14 +68,19 @@ function CardSection() {
           </div>
         </div>
         <div className={`${styles['card']}`}>
-          <div>
-            <MovingIcon />
+          <div className={`${styles['icon-wrapper']}`}>
+            <Image src={Icon4} alt="icon" />
           </div>
           <div>
             <h4>Get rewarded with LGCN tokens</h4>
             <p>Get rewarded with Legacy Network tokens (LGCN).</p>
           </div>
         </div>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'center', marginTop: '2rem'}}>
+        <Link href="/">
+          <a className='button-blue'>Learn More</a>
+        </Link>
       </div>
       <h4 className="spacing-x">
         Expand your knowledge and build your dream life with the help of our AI.
