d28b4980324192a870031ebdf87c837efac82cbc
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-09-28 12:56:55 +0200

    made card section cards half transparent with blur

---
+++ components/Home/LogoParticles/LogoParticles.jsx
@@ -0,0 +1,46 @@
+import styles from './logoparticles.module.scss';
+import ParticleImage, {
+  ParticleOptions,
+  Vector,
+  forces,
+  ParticleForce
+} from "react-particle-image";
+
+const particleOptions = {
+  filter: ({ x, y, image }) => {
+    // Get pixel
+    const pixel = image.get(x, y);
+    // Make a particle for this pixel if blue > 50 (range 0-255)
+    return pixel.b > 50;
+  },
+  color: ({ x, y, image }) => "rgb(30, 79, 188)",
+  // radius: () => Math.random() * 1.5 + 0.5,
+  radius: () => Math.random() * 1.5 + 0.5,
+  mass: () => 40,
+  friction: () => 0.15,
+  initialPosition: ({ canvasDimensions }) => {
+    return new Vector(canvasDimensions.width / 2, canvasDimensions.height / 2);
+  }
+};
+
+const motionForce = (x, y) => {
+  return forces.disturbance(x, y, 50);
+};
+
+function LogoParticles() {
+  
+  return (
+    <ParticleImage
+      src={"/logo-cropped.png"}
+      scale={0.75}
+      entropy={20}
+      maxParticles={2000}
+      particleOptions={particleOptions}
+      mouseMoveForce={motionForce}
+      touchMoveForce={motionForce}
+      backgroundColor="transparent"
+    />
+  )
+}
+
+export default LogoParticles
