a13e6ee08c458326af218111036a3c2dbc556bf1
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-10-01 10:46:06 +0200

    updated personal development page content

--- components/PersonalDevelopment/Explanation/Explanation.jsx
+++ components/PersonalDevelopment/Explanation/Explanation.jsx
@@ -13,24 +13,26 @@ function Explanation() {
         <Image src={Grid} alt="grid" />
       </div>
       <div>
-        <h2>What is Personal Development</h2>
+        <h3>What is personal development about?</h3>
         <p>
-          Personal development is a lifelong process. It is a way for people to assess their skills and qualities, consider their aims in life and set goals in order to realise and maximise their potential.
+          Personal development is about gaining a deep understanding of our own psyche as well as human nature in general. The goal is to find harmful traits in ourselves, understand how they prevent us from succeeding, and replace them step by step with more effective traits. During this developmental journey, we can find ourselves, overcome our fears and weaknesses, develop life-changing skills and unlock our full potential.
+        </p>
+      </div>
+      <div style={{marginBottom: '1.5rem'}}>
+        <h3>Why is it important?</h3>
+        <p>
+          We humans tend to believe that we don&apos;t have much control over our destiny. We think that our potential is as limited as our possibilities are. In reality, these beliefs are an illusion. Our success/happiness is a direct reflection of our personality and our personality is a direct reflection of our individual programming.
+        </p>
+        <p>
+          This programming includes our beliefs, thoughts, habits, behavior patterns and our neurological conditioning. It controls everything. The way we approach certain challenges, how quickly we recover from setbacks, how we react to different situations etc. It also determines our level of self-confidence, self-love, resilience, assertiveness, discipline, emotional control, and more.
         </p>
       </div>
       <div>
-        <h3>The importance of Personal Development</h3>
+        <h3>What can be achieved? </h3>
         <ol>
-          <li>Personal growth is necessary for boosting confidence</li>
-          <li>Self-development strengthens your skills</li>
-          <li>It gives clarity of vision</li>
-          <li>Improving yourself motivates a person</li>
-          <li>It provides satisfaction</li>
-          <li>It helps in career growth</li>
-          <li>Essential to become happy</li>
-          <li>Helps you get more opportunities</li>
-          <li>Helps your talent shines</li>
-          <li>Helps you in creating a positive attitude towards life</li>
+          <li>Get to know yourself on a deeper level and gain control over your subconscious reactions and approaches.</li>
+          <li>Learn how certain traits are connected to different outcomes and readjust where it’s needed.</li>
+          <li>Sustainably improve all areas of your life such as career and finance, platonic relationships, romantic relationships, mental health, physical health and much more.</li>
         </ol>
       </div>
     </div>
