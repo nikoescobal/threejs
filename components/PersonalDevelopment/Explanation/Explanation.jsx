e00acd0471ebcca6fc9803e2baa04c09107860f7
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-10-02 04:22:42 +0200

    updated personal development page to be widescreen responsive

--- components/PersonalDevelopment/Explanation/Explanation.jsx
+++ components/PersonalDevelopment/Explanation/Explanation.jsx
@@ -12,13 +12,13 @@ function Explanation() {
       <div className={styles['image-wrapper']}>
         <Image src={Grid} alt="grid" />
       </div>
-      <div>
+      <div className={`max-1920`}>
         <h3>What is personal development about?</h3>
         <p>
           Personal development is about gaining a deep understanding of our own psyche as well as human nature in general. The goal is to find harmful traits in ourselves, understand how they prevent us from succeeding, and replace them step by step with more effective traits. During this developmental journey, we can find ourselves, overcome our fears and weaknesses, develop life-changing skills and unlock our full potential.
         </p>
       </div>
-      <div style={{marginBottom: '1.5rem'}}>
+      <div style={{marginBottom: '1.5rem'}} className={`max-1920`}>
         <h3>Why is it important?</h3>
         <p>
           We humans tend to believe that we don&apos;t have much control over our destiny. We think that our potential is as limited as our possibilities are. In reality, these beliefs are an illusion. Our success/happiness is a direct reflection of our personality and our personality is a direct reflection of our individual programming.
@@ -27,7 +27,7 @@ function Explanation() {
           This programming includes our beliefs, thoughts, habits, behavior patterns and our neurological conditioning. It controls everything. The way we approach certain challenges, how quickly we recover from setbacks, how we react to different situations etc. It also determines our level of self-confidence, self-love, resilience, assertiveness, discipline, emotional control, and more.
         </p>
       </div>
-      <div>
+      <div className={`max-1920`}>
         <h3>What can be achieved? </h3>
         <ol>
           <li>Get to know yourself on a deeper level and gain control over your subconscious reactions and approaches.</li>
--- components/PersonalDevelopment/HealthCrisis/HealthCrisis.jsx
+++ components/PersonalDevelopment/HealthCrisis/HealthCrisis.jsx
@@ -30,7 +30,7 @@ function HealthCrisis() {
           <DonutChart />  
         </div>
         <button type='button' className='button-blue'>More about mental health crisis</button>
-        <h3 style={{marginBottom: '2rem'}}>The pillar of an unhappy life</h3>
+        <h3 style={{marginBottom: '2rem'}}>The pillars of an unhappy life</h3>
         <div className={styles['pillar-wrapper']}>
           <Image src={Pillar} alt='pillar of an unhappy life'/>
         </div>
