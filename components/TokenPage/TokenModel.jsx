dc6b08f3bece6230da31aa53ae9a1fe8fcbbc5dc
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-10-06 03:56:26 +0200

    fixed issues with token materials

--- components/TokenPage/TokenModel.jsx
+++ components/TokenPage/TokenModel.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unknown-property */
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls, useGLTF, useTexture, Environment } from '@react-three/drei';
 import {
   useRef, Suspense, useMemo, useLayoutEffect,
   useState, useEffect,
@@ -8,49 +8,74 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import {
   Canvas, useLoader, useFrame, useThree,
 } from '@react-three/fiber';
-import { MeshPhysicalMaterial, MeshStandardMaterial, TextureLoader } from 'three';
+import {
+  MeshPhysicalMaterial,
+  MeshStandardMaterial,
+  TextureLoader,
+  CubeCamera,
+  CubeTextureLoader,
+  WebGLCubeRenderTarget,
+  RGBFormat,
+  LinearMipmapLinearFilter,
+ } from 'three';
 import { useMediaQuery } from '@mui/material';
 import styles from './tokenmodel.module.scss';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 
+const CameraControls = () => {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+
+  const controls = useRef();
+  useFrame(() => controls.current.update());
+
+  return (
+    <OrbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      autoRotate={true}
+      enableZoom={false}
+      enablePan={false}
+    />
+  )
+}
+
 function Token() {
+  const { scene, gl } = useThree();
   const ref = useRef();
-  // const gltf = useLoader(GLTFLoader, '/3d/ln_token_3d.gltf');
-  // const gltf = useLoader(GLTFLoader, '/3d/Token_Updated.glb');
-  const gltf = useLoader(GLTFLoader, '/3d/Token_Updated-small.glb', loader => {
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/');
-    loader.setDRACOLoader(dracoLoader);
-   });
-  const clonedScene = useMemo(() => gltf.scene.clone(), []);
+  const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
+    format: RGBFormat,
+    generateMipmaps: true,
+    minFilter: LinearMipmapLinearFilter,
+  })
 
-  useLayoutEffect(() => {
-    clonedScene.traverse((o) => {
-      if (o.type === 'Mesh') {
-        o.material = new MeshPhysicalMaterial(
-          {
-            color: 0x1e4fbc,
-            roughness: 0.25,
-            metalness: 1.25,
-            sheenColor: 0x1e4fbc,
-            sheen: 0.5,
-            ior: 1.45,
-            specularColor: 0xffffff,
-            specularIntensity: 0.5,
-            emissive: 0x1e4fbc,
-            emissiveIntensity: 0.5
-          },
-        );
-      }
-    });
-  }, [clonedScene]);
+  const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
+  cubeCamera.position.set(0, 100, 0)
+  scene.add(cubeCamera);
 
-  useFrame(() => {
-    ref.current.rotation.y += 0.01;
-  });
+  useFrame(() => cubeCamera.update(gl, scene))
+  
+  // const gltf = useLoader(GLTFLoader, '/3d/newtoken/coin.glb');
+  const { materials, scene: objScene } = useLoader(GLTFLoader, '/3d/newtoken/coin.glb');
+  // const clonedScene = useMemo(() => gltf.scene.clone(), []);
   const { viewport } = useThree();
   const [scale, setScale] = useState();
   const isDesktop = useMediaQuery('(min-width: 1024px)');
+  const [colorMap] = useLoader(TextureLoader, [
+    '/3d/newtoken/BaseColor.png'
+  ])
+
+  useLayoutEffect(() => {
+    if (materials && scene) {
+      // Object.assign(materials.Material, {
+      //   map: colorMap,
+      //   envMap: cubeCamera.renderTarget.texture 
+      // })
+      
+    }
+  }, [scene, materials])
 
   useEffect(() => {
     if (isDesktop) {
@@ -59,10 +84,70 @@ function Token() {
       setScale(viewport.width / 180);
     }
   }, [isDesktop]);
+
+  // useMemo(() => {
+  //   for (const material in materials) {
+  //     // iterate the materials
+  //     if (Object.prototype.hasOwnProperty.call(materials, material)) {
+  //       // change the color of all the materials (there's only one in this model)
+  //       // materials[material].color.set("#bb6f3e");
+        
+  //       // you can also change the color of a specific material if you know the name of the material
+  //     }
+  //   }
+  // }, [materials]);
+
+
+  
+  // const { nodes, materials } = useGLTF("/3d/newtoken/coin.glb");
+  // console.log('nodes', nodes);
+  // console.log('gltf', gltf);
   return (
-    <primitive scale={scale} ref={ref} object={clonedScene} position={[0, 0, 0]} />
+  //   <group dispose={null}>
+  //   <group position={[0, 0, -0.01]} rotation={[0, 0, 0.01]}>
+  //     <mesh
+  //       castShadow
+  //       receiveShadow
+  //       geometry={nodes.Cube_10.geometry}
+  //       material={materials["coin.001"]}
+  //       // material-color="#1e4fbc"
+  //       position={[0,0,0]}
+  //       rotation={[0.8, 0.66, 0.72]}
+  //       scale={0.8}
+  //     >
+  //       {/* <meshPhysicalMaterial  /> */}
+  //       </mesh>
+  //   </group>
+  // </group>
+
+
+    <primitive scale={scale} ref={ref} object={objScene} position={[0, 0, 0]} />
   );
 }
+
+useGLTF.preload("/3d/newtoken/coin.glb");
+
+function SkyBox() {
+  const { scene } = useThree();
+  const loader = new CubeTextureLoader();
+  const texture = loader.load([
+    '/3d/skybox/1.jpg',
+    '/3d/skybox/2.jpg',
+    '/3d/skybox/3.jpg',
+    '/3d/skybox/4.jpg',
+    '/3d/skybox/5.jpg',
+    '/3d/skybox/6.jpg',
+    // "https://6izyu.csb.app/4b.jpg",
+    // "https://6izyu.csb.app/3.jpg",
+    // "https://6izyu.csb.app/4b.jpg",
+    // "https://6izyu.csb.app/4.jpg",
+    // "https://6izyu.csb.app/5.jpg",
+    // "https://6izyu.csb.app/2.jpg",
+  ]);
+  scene.background = texture;
+  return null
+}
+
 
 function Scene() {
   return (
@@ -70,15 +155,17 @@ function Scene() {
       {/* <ambientLight intensity={1} />
       <pointLight intensity={5} color={0xeeeeee} position={[30, 30, 10]} />
       <pointLight intensity={5} position={[-30, 30, -15]} /> */}
-      <directionalLight intensity={1} position={[5, 2, 5]} scale={0.5}/>
-      <directionalLight intensity={1} position={[-20, 2, -1]} scale={0.5} />
+      {/* <directionalLight intensity={1} position={[5, 2, 5]} scale={0.5}/>
+      <directionalLight intensity={1} position={[-20, 2, -1]} scale={0.5} /> */}
       <Suspense fallback={null}>
         <Token />
       </Suspense>
+      {/* <SkyBox /> */}
       <OrbitControls enableZoom={false} />
     </>
   );
 }
+
 
 function TokenModel() {
   return (
@@ -89,7 +176,15 @@ function TokenModel() {
         }}
         className={`${styles['token_canvas']}`}
       >
-        <Scene />
+        {/* <Scene /> */}
+      {/* <ambientLight intensity={1} /> */}
+
+        {/* <directionalLight intensity={1} position={[5, 2, 5]} scale={0.5}/> */}
+      {/* <directionalLight intensity={1} position={[-20, 2, -1]} scale={0.5} /> */}
+        <CameraControls />
+        <Token />
+        <Environment preset='studio' />
+        {/* <SkyBox /> */}
       </Canvas>
     </div>
   )
