1c57341a210e2e03adbe40bf0cb4e0722f8d02f5
Nikolas Escobal <niko.escobal@gmail.com>
2022-10-15 15:47:08 +0800

    Changed title of utility token

--- components/TokenPage/SidePressureSection/SidePressureSection.jsx
+++ components/TokenPage/SidePressureSection/SidePressureSection.jsx
@@ -6,13 +6,23 @@ import CloudIcon from '@mui/icons-material/Cloud';
 
 function SidePressureSection() {
   const { isDarkMode } = useStore();
-  
+
   return (
-    <section className={`${styles.section} ${generics['spacing-x']} ${!isDarkMode ? `${styles.light}` : ''} max-1920`}>
+    <section
+      className={`${styles.section} ${generics['spacing-x']} ${
+        !isDarkMode ? `${styles.light}` : ''
+      } max-1920`}
+    >
       <div className={styles['text-wrapper']}>
         <h3>A real utility token depegged from the market</h3>
         <p>
-          A defined real utility token is depegged from on-going market conditions and shouldn&apos;t necessarily crash in a bear market. For this reason we aim to create a token with constant, non-speculative buy side pressure. We achieve this by offering several attractive utilities which aren&apos;t directly related to the crypto industry. These utilities can only be accessed by using LGCN token and the demand for them stays the same or even grows within a bear market.
+          A defined real utility token is depegged from on-going market
+          conditions and shouldn&apos;t necessarily crash in a bear market. For
+          this reason we aim to create a token with constant, non-speculative
+          buy side pressure. We achieve this by offering several attractive
+          utilities which aren&apos;t directly related to the crypto industry.
+          These utilities can only be accessed by using LGCN token and the
+          demand for them stays the same or even grows within a bear market.
         </p>
       </div>
       <div className={`${styles['cards-wrapper']}`}>
@@ -48,8 +58,10 @@ function SidePressureSection() {
             <img src="/icons/bsp4.svg" alt="" />
           </div>
           <div>
-            <h4>Non-crypto utility</h4>
-            <span>The demand for our utility doesn&apos;t decrease in a bear-market.</span>
+            <h4>Constant demand utility</h4>
+            <span>
+              The demand for our utility doesn&apos;t decrease in a bear-market.
+            </span>
           </div>
         </div>
         <div className={`${styles['card']}`}>
@@ -58,12 +70,14 @@ function SidePressureSection() {
           </div>
           <div>
             <h4>More in future</h4>
-            <span>More exciting features will be announced on a regular basis!</span>
+            <span>
+              More exciting features will be announced on a regular basis!
+            </span>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default SidePressureSection
+export default SidePressureSection;
