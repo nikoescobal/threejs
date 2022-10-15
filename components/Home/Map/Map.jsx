694dce2ba663d9aa71c32105d3648e986d7a81f7
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-10-15 11:50:32 +0200

    added zoomed in world map

--- components/Home/Map/Map.jsx
+++ components/Home/Map/Map.jsx
@@ -1,16 +1,24 @@
+import dynamic from 'next/dynamic';
 import useStore from '../../../store/store';
 import styles from './map.module.scss';
+// import MapParticles from './MapParticles';
 
 function Map() {
   const { isDarkMode } = useStore((state) => state);
-  
+  const MapParticles = dynamic(
+    () => import('./MapParticles'),
+    { ssr: false }
+  )
   return (
-    <section className={`${styles.map} ${!isDarkMode ? `${styles.light}` : ''} max-1920`}>
-      <div>
-      <img src="/backgrounds/mapbase.png" alt="" />
+    <section className={styles.wrapper}>
+      <div className={`${styles.map} ${!isDarkMode ? `${styles.light}` : ''} max-1920`}>
+        {/* <div>
+        <img src="/backgrounds/mapbase.png" alt="" />
 
+        </div> */}
+        <h4>Join Us To Make The world A Better Place</h4>
       </div>
-      <h4>Join Us To Make The world A Better Place</h4>
+      <MapParticles className={styles.particles} />
     </section>
   )
 }
