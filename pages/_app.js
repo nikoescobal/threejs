1a7532018e1debaa7b6a29cf79c9c49dbf3b95eb
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-10-16 00:10:10 +0200

    added script for google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ import '../styles/apexcharts.module.scss';
 import Footer from '../components/Footer/Footer';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import { useEffect } from 'react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   const { isDarkMode } = useStore((state) => state);
@@ -22,6 +23,19 @@ function MyApp({ Component, pageProps }) {
   
   return (
     <div className={`content-wrapper ${isDarkMode ? '' : 'light'} `}>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script id="analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      
       <ParallaxProvider>
         <Navbar />
         <Component {...pageProps} />
