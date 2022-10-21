9983f5827a48952fe01a47d4b99d31bced8fb84b
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-10-21 13:53:28 +0200

    ported credentials

--- components/Home/NewsLetter/NewsLetter.jsx
+++ components/Home/NewsLetter/NewsLetter.jsx
@@ -1,10 +1,45 @@
 import styles from './newsletter.module.scss';
 import generics from '../../../styles/generics.module.scss';
 import useStore from '../../../store/store';
+import {collection,QueryDocumentSnapshot,DocumentData,query,where,limit,getDocs, doc, setDoc, addDoc} from "firebase/firestore";
+import { firestore } from '../../../firebase/clientapp';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function NewsLetter() {
   const { isDarkMode } = useStore((state) => state);
+  const newsletterCollection = collection(firestore, 'email-subscribers');
+  const [emails, setEmails] = useState([]);
+  const [email, setEmail] = useState([]);
 
+  const getEmails = async () => {
+    const newsLetterQuery = query(newsletterCollection);
+    const querySnapshot = await getDocs(collection(firestore, "email-subscribers"));
+    
+    const result = [];
+    querySnapshot.forEach((snapshot) => {
+      result.push(snapshot.data());
+    });
+    setEmails(result);
+  };
+
+  function saveEmail() {
+    addDoc(collection(firestore, "email-subscribers"), {
+      email
+    })
+    .then(() => {
+      console.log('success');
+    })
+    .catch(() => {
+      console.log('error');
+    })
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    saveEmail(email);
+  }
+  
   return (
     <div
       className={`${styles['news-letter']} ${generics['spacing-x']} ${
@@ -16,8 +51,8 @@ function NewsLetter() {
         <p>
           Subscribe to our newsletter and be the first to get updates on our products and services. 
         </p>
-        <form>
-          <input type="email" name="email" placeholder="Email" id="" />
+        <form onSubmit={handleSubmit}>
+          <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder="Email" id="" />
           <button type="submit" className="button-blue">Subscribe</button>
         </form>
       </div>
