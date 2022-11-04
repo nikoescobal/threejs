cca7a4c474951b8e82c839785d4c72c8e2156e80
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-11-04 13:41:25 +0200

    added links to partner section

--- components/Generic/Partners/Partners.jsx
+++ components/Generic/Partners/Partners.jsx
@@ -8,38 +8,45 @@ function Partners() {
       endpoint: 'crypto-valley.png',
       alt: 'crypto valley logo',
       class: 'crypto-valley',
+      src: 'https://cryptovalley.swiss/'
     },
     {
       endpoint: 'cv-labs.png',
       alt: 'cv labs logo',
       class: 'cv-labs',
+      src: 'https://www.cvlabs.com/'
     },
     {
       endpoint: 'niedermuller.png',
       alt: 'niedermuller logo',
       class: 'niedermuller',
+      src: 'https://niedermueller.law/de/'
     },
     {
       endpoint: 'sercor.png',
       alt: 'seratio logo',
       class: 'sercor',
+      src: 'https://www.sercor.com/en/'
     },
     {
       endpoint: 'synaps.png',
       alt: 'synaps logo',
       class: 'synaps',
+      src: 'https://synaps.io/'
     },
     {
       endpoint: 'caag.png',
       alt: 'caag logo',
       class: 'caag',
+      src: 'https://cryptoadvisory.li/en/home/'
     },
     {
       endpoint: 'red4sec.png',
       alt: 'red4sec logo',
       class: 'red4sec',
+      src: 'https://red4sec.com/en'
     },
-    // {
+  // {
     //   endpoint: 'maerki.png',
     //   alt: 'maerki logo',
     //   class: 'maerki',
@@ -48,16 +55,19 @@ function Partners() {
       endpoint: 'saynode.png',
       alt: 'saynode logo',
       class: 'saynode',
+      src: 'https://saynode.ch/'
     },
     {
       endpoint: 'safehaven.png',
       alt: 'safehaven logo',
       class: 'safehaven',
+      src: 'https://safehaven.io/'
     },
     {
       endpoint: 'vechain.png',
       alt: 'vechain logo',
       class: 'vechain',
+      src: 'https://www.vechain.org/'
     },
   ];
   const { isDarkMode } = useStore((state) => state);
@@ -72,11 +82,13 @@ function Partners() {
       >
         {partners.map((logo) => (
           <div key={logo.alt}>
-            <img
-              src={`/logos/${logo.endpoint}`}
-              alt={logo.alt}
-              className={styles[logo.class]}
-            />
+            <a href={logo.src} target="_blank" rel="noopener noreferrer">
+              <img
+                src={`/logos/${logo.endpoint}`}
+                alt={logo.alt}
+                className={styles[logo.class]}
+              />
+            </a>
           </div>
         ))}
       </div>
