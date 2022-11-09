0be4c071b587da6ded61ba98db8d7977133b8414
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-11-09 04:31:04 +0200

    added code for mailchimp

--- components/Home/NewsLetter/NewsLetter.jsx
+++ components/Home/NewsLetter/NewsLetter.jsx
@@ -2,6 +2,7 @@ import styles from './newsletter.module.scss';
 import generics from '../../../styles/generics.module.scss';
 import useStore from '../../../store/store';
 import {collection,QueryDocumentSnapshot,DocumentData,query,where,limit,getDocs, doc, setDoc, addDoc} from "firebase/firestore";
+import axios from 'axios';
 import { firestore } from '../../../firebase/clientapp';
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -12,6 +13,11 @@ function NewsLetter() {
   const [emails, setEmails] = useState([]);
   const [email, setEmail] = useState('');
 
+  const API_KEY = process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY
+  const API_SERVER = process.env.NEXT_PUBLIC_MAILCHIMP_API_SERVER
+  const AUDIENCE_ID = process.env.NEXT_PUBLIC_MAILCHIMP_AUDIENCE_ID
+  
+  
   const getEmails = async () => {
     const newsLetterQuery = query(newsletterCollection);
     const querySnapshot = await getDocs(collection(firestore, "email-subscribers"));
@@ -38,7 +44,31 @@ function NewsLetter() {
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    saveEmail(email);
+    // saveEmail(email);
+    const url = `https://${API_SERVER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`
+
+    const base64ApiKey = Buffer.from(`anystring:${API_KEY}`).toString("base64");
+    
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        Authorization: `Basic ${base64ApiKey}`
+      }
+    }
+    
+    const data = {
+      email_address: email,
+      status: 'subscribed'
+    }
+  
+    axios.post(url, data, options)
+    .then((res) => {
+      console.log('subscribed');
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   }
   
   return (
