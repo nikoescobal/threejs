e7506e51d9197d601042bb228b7609c1afe35d96
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-11-25 18:32:39 +0200

    replaced gradient with webp format

--- components/Generic/Gradient/Gradient.jsx
+++ components/Generic/Gradient/Gradient.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import Image from 'next/image'
 import styles from './gradient.module.scss'
-import GradientImg from '../../../public/backgrounds/gradient.png'
+import GradientImg from '../../../public/backgrounds/gradient.webp'
 import GradientImgHalf from '../../../public/backgrounds/gradient-cut.png'
 import useStore from '../../../store/store'
 
