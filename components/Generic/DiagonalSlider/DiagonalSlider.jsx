3a1d6438f69702606130eecd1de8f8f72651485d
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-12-09 15:51:52 +0200

    updated roadmap

--- components/Generic/DiagonalSlider/DiagonalSlider.jsx
+++ components/Generic/DiagonalSlider/DiagonalSlider.jsx
@@ -15,47 +15,47 @@ function DiagonalSlider({ showBenefits }) {
   const roadmapItems = [
     {
       phase: 'Phase 1',
-      date: 'August',
+      date: 'Q3 2022',
       content: [
         {
           id: 'r1',
-          content: 'Token smart contract development for all 4 blockchains',
-          isChecked: true,
-        },
-        {
-          id: 'r2',
-          content: 'Token smart contracts first security audit',
-          isChecked: true,
-        },
-        {
-          id: 'r3',
           content: 'Pre-production of social-media content for the next months',
           isChecked: true,
         },
         {
+          id: 'r2',
+          content: 'Token smart contract development for all 4 blockchains',
+          isChecked: true,
+        },
+        {
+          id: 'r3',
+          content: 'Wallet concept and user interface finished',
+          isChecked: true,
+        },
+        {
           id: 'r4',
-          content: 'Pre-launch app concept and user interface finished',
+          content: 'Token smart contracts first security audit',
           isChecked: true,
         },
       ],
     },
     {
       phase: 'Phase 2',
-      date: 'September',
+      date: 'Q3 2022',
       content: [
         {
           id: 'r5',
-          content: 'Launch of our new website',
+          content: 'Announcement of new utilities',
           isChecked: true,
         },
         {
           id: 'r6',
-          content: 'Announcement of new utilities and strategies',
+          content: 'Launch of our new website',
           isChecked: true,
         },
         {
           id: 'r7',
-          content: 'Alpha testing of the pre-launch app',
+          content: 'Alpha testing of the wallet',
           isChecked: true,
         },
         {
@@ -67,16 +67,16 @@ function DiagonalSlider({ showBenefits }) {
     },
     {
       phase: 'Phase 3',
-      date: 'October',
+      date: 'Q4 2022',
       content: [
         {
           id: 'r9',
-          content: 'Launch of our blog / documentation page',
-          isChecked: true,
+          content: 'Start of intensive community building along with events',
+          isChecked: false,
         },
         {
           id: 'r10',
-          content: 'Start of the first social-media campaign',
+          content: 'Start of our web-based giveaway and referral program',
           isChecked: false,
         },
         {
@@ -86,38 +86,38 @@ function DiagonalSlider({ showBenefits }) {
         },
         {
           id: 'r12',
-          content: 'Announcement of new strategical partnerships',
+          content: 'Launch of our blog / documentation page',
           isChecked: true,
         },
         {
           id: 'r13',
-          content: 'Announcement of community web-board functions',
-          isChecked: true,
+          content: 'Start of the first social-media campaign ',
+          isChecked: false,
         },
         {
           id: 'r14',
-          content: 'Announcement of partners web-board functions',
-          isChecked: true,
+          content: 'Opening of our community channels',
+          isChecked: false,
         },
       ],
     },
     {
       phase: 'Phase 4',
-      date: 'November',
+      date: 'Q4 2022',
       content: [
         {
           id: 'r15',
-          content: 'Step by step opening of our community channels',
+          content: 'LGCT listing on the SafeSwap multi-chain bridge',
           isChecked: false,
         },
         {
           id: 'r16',
-          content: 'Start of intensive community building along with events',
+          content: 'Token deployment on the blockchains',
           isChecked: false,
         },
         {
           id: 'r17',
-          content: 'Start of our web-based giveaway and referral program',
+          content: 'Security audits of all smart contracts',
           isChecked: false,
         },
         {
@@ -125,36 +125,36 @@ function DiagonalSlider({ showBenefits }) {
           content: 'Beta testing of the Legacy Wallet',
           isChecked: false,
         },
-      ],
-    },
-    {
-      phase: 'Phase 5',
-      date: 'December',
-      content: [
         {
           id: 'r19',
           content: 'Start of our YouTube channel',
           isChecked: false,
         },
+      ],
+    },
+    {
+      phase: 'Phase 5',
+      date: 'Q1 2023',
+      content: [
         {
           id: 'r20',
-          content: 'Security audits of all smart-contracts',
+          content: 'Legacy Wallet release for IOS and Android',
           isChecked: false,
         },
         {
           id: 'r21',
-          content: 'LEGACY Wallet release for IOS and Android',
+          content: 'Launch of the community web-board',
           isChecked: false,
         },
         {
           id: 'r22',
-          content: 'Token deployment on the blockchains',
+          content: 'Launch of the partners web-board',
           isChecked: false,
         },
         {
           id: 'r23',
           content:
-            'Legacy Token (LGCT) listing on the SafeSwap multi-chain bridge',
+            'Pre-listing on coinmarketcap',
           isChecked: false,
         },
       ],
@@ -165,47 +165,17 @@ function DiagonalSlider({ showBenefits }) {
       content: [
         {
           id: 'r24',
-          content: 'Launch of the community web-board (basic functions)',
-          isChecked: false,
-        },
-        {
-          id: 'r25',
-          content: 'Launch of the partners web-board (basic functions)',
-          isChecked: false,
-        },
-        {
-          id: 'r26',
-          content: 'Public sale start (depending on the markets)',
-          isChecked: false,
-        },
-        {
-          id: 'r27',
-          content: 'Alpha testing of our main app',
-          isChecked: false,
-        },
-        {
-          id: 'r28',
           content: 'Beta testing of the main app',
           isChecked: false,
         },
         {
-          id: 'r29',
-          content: 'Pre-listing on coinmarketcap',
+          id: 'r25',
+          content: 'Legacy NFT mint',
           isChecked: false,
         },
         {
-          id: 'r30',
-          content: 'Legacy Token (LGCT) listing on a centralized exchange',
-          isChecked: false,
-        },
-        {
-          id: 'r31',
-          content: 'Launch of the first version of our main app',
-          isChecked: false,
-        },
-        {
-          id: 'r32',
-          content: 'End of the web-based giveaway',
+          id: 'r26',
+          content: 'Public sale start',
           isChecked: false,
         },
       ],
@@ -215,8 +185,28 @@ function DiagonalSlider({ showBenefits }) {
       date: 'Q2 2023',
       content: [
         {
-          id: 'r33',
-          content: 'To be continued...',
+          id: 'r27',
+          content: 'Launch of the first version of our main app',
+          isChecked: false,
+        },
+        {
+          id: 'r28',
+          content: 'LGCT listing on a centralized exchange',
+          isChecked: false,
+        },
+        {
+          id: 'r29',
+          content: 'Introduction of new App-Features',
+          isChecked: false,
+        },
+        {
+          id: 'r30',
+          content: 'End of the web-based giveaway',
+          isChecked: false,
+        },
+        {
+          id: 'r31',
+          content: 'Introduction of new Roadmap',
           isChecked: false,
         },
       ],
