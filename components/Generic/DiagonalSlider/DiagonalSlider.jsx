825f63ad7787aaa8290db724c548a0d965a18656
El-Potato-Slayer <leighchad.cl@gmail.com>
2022-12-09 07:10:56 +0200

    fixed key issue for list mapping

--- components/Generic/DiagonalSlider/DiagonalSlider.jsx
+++ components/Generic/DiagonalSlider/DiagonalSlider.jsx
@@ -18,18 +18,22 @@ function DiagonalSlider({ showBenefits }) {
       date: 'August',
       content: [
         {
+          id: 'r1',
           content: 'Token smart contract development for all 4 blockchains',
           isChecked: true,
         },
         {
+          id: 'r2',
           content: 'Token smart contracts first security audit',
           isChecked: true,
         },
         {
+          id: 'r3',
           content: 'Pre-production of social-media content for the next months',
           isChecked: true,
         },
         {
+          id: 'r4',
           content: 'Pre-launch app concept and user interface finished',
           isChecked: true,
         },
@@ -40,18 +44,22 @@ function DiagonalSlider({ showBenefits }) {
       date: 'September',
       content: [
         {
+          id: 'r5',
           content: 'Launch of our new website',
           isChecked: true,
         },
         {
+          id: 'r6',
           content: 'Announcement of new utilities and strategies',
           isChecked: true,
         },
         {
+          id: 'r7',
           content: 'Alpha testing of the pre-launch app',
           isChecked: true,
         },
         {
+          id: 'r8',
           content: 'Expansion of our teams',
           isChecked: true,
         },
@@ -62,26 +70,32 @@ function DiagonalSlider({ showBenefits }) {
       date: 'October',
       content: [
         {
+          id: 'r9',
           content: 'Launch of our blog / documentation page',
           isChecked: true,
         },
         {
+          id: 'r10',
           content: 'Start of the first social-media campaign',
           isChecked: false,
         },
         {
+          id: 'r11',
           content: 'Attending several blockchain events around the globe',
           isChecked: true,
         },
         {
+          id: 'r12',
           content: 'Announcement of new strategical partnerships',
           isChecked: true,
         },
         {
+          id: 'r13',
           content: 'Announcement of community web-board functions',
           isChecked: true,
         },
         {
+          id: 'r14',
           content: 'Announcement of partners web-board functions',
           isChecked: true,
         },
@@ -92,18 +106,22 @@ function DiagonalSlider({ showBenefits }) {
       date: 'November',
       content: [
         {
+          id: 'r15',
           content: 'Step by step opening of our community channels',
           isChecked: false,
         },
         {
+          id: 'r16',
           content: 'Start of intensive community building along with events',
           isChecked: false,
         },
         {
+          id: 'r17',
           content: 'Start of our web-based giveaway and referral program',
           isChecked: false,
         },
         {
+          id: 'r18',
           content: 'Beta testing of the Legacy Wallet',
           isChecked: false,
         },
@@ -114,22 +132,27 @@ function DiagonalSlider({ showBenefits }) {
       date: 'December',
       content: [
         {
+          id: 'r19',
           content: 'Start of our YouTube channel',
           isChecked: false,
         },
         {
+          id: 'r20',
           content: 'Security audits of all smart-contracts',
           isChecked: false,
         },
         {
+          id: 'r21',
           content: 'LEGACY Wallet release for IOS and Android',
           isChecked: false,
         },
         {
+          id: 'r22',
           content: 'Token deployment on the blockchains',
           isChecked: false,
         },
         {
+          id: 'r23',
           content:
             'Legacy Token (LGCT) listing on the SafeSwap multi-chain bridge',
           isChecked: false,
@@ -141,38 +164,47 @@ function DiagonalSlider({ showBenefits }) {
       date: 'Q1 2023',
       content: [
         {
+          id: 'r24',
           content: 'Launch of the community web-board (basic functions)',
           isChecked: false,
         },
         {
+          id: 'r25',
           content: 'Launch of the partners web-board (basic functions)',
           isChecked: false,
         },
         {
+          id: 'r26',
           content: 'Public sale start (depending on the markets)',
           isChecked: false,
         },
         {
+          id: 'r27',
           content: 'Alpha testing of our main app',
           isChecked: false,
         },
         {
+          id: 'r28',
           content: 'Beta testing of the main app',
           isChecked: false,
         },
         {
+          id: 'r29',
           content: 'Pre-listing on coinmarketcap',
           isChecked: false,
         },
         {
+          id: 'r30',
           content: 'Legacy Token (LGCT) listing on a centralized exchange',
           isChecked: false,
         },
         {
+          id: 'r31',
           content: 'Launch of the first version of our main app',
           isChecked: false,
         },
         {
+          id: 'r32',
           content: 'End of the web-based giveaway',
           isChecked: false,
         },
@@ -183,6 +215,7 @@ function DiagonalSlider({ showBenefits }) {
       date: 'Q2 2023',
       content: [
         {
+          id: 'r33',
           content: 'To be continued...',
           isChecked: false,
         },
@@ -248,63 +281,32 @@ function DiagonalSlider({ showBenefits }) {
           }vw, ${position.y * offset.y}vw)`,
         }}
       >
-        {!showBenefits &&
+        {
           roadmapItems.map((item, index) => (
-            <>
-              <div
-                key={item.phase}
-                className={`${styles['slide']} ${
-                  index === Math.abs(position.x - 1)
-                    ? `${styles['active']}`
-                    : ''
-                }`}
-                style={{
-                  transform: `translateY(${offset.y * index}vw)`,
-                }}
-              >
-                <div className={`${styles['phase-wrapper']}`}>
-                  <p>{item.phase}</p>
-                </div>
-                <div className={`${styles['timeframe-wrapper']}`}>
-                  <p>{item.date}</p>
-                </div>
-                <SliderContent
-                  list={item.content}
-                  showBenefits={showBenefits}
-                  isActive={index === Math.abs(position.x - 1)}
-                />
-                <div></div>
-              </div>
-            </>
-          ))}
-        {showBenefits &&
-          benefits.map((item, index) => (
-            <>
-              <div
-                key={item.phase}
-                className={`${styles['slide']} ${
-                  index === Math.abs(position.x - 1)
-                    ? `${styles['active']}`
-                    : ''
-                }`}
-                style={{
-                  transform: `translateY(${offset.y * index}vw)`,
-                }}
-              >
-                <div className={`${styles['phase-wrapper']}`}>
-                  <p>{item.phase}</p>
-                </div>
-                <div className={`${styles['timeframe-wrapper']}`}>
-                  <p>{item.date}</p>
-                </div>
-                <SliderContent
-                  list={item.content}
-                  isActive={index === Math.abs(position.x - 1)}
-                  showBenefits={showBenefits}
-                />
-                <div></div>
-              </div>
-            </>
+          <div
+            key={item.phase}
+            className={`${styles['slide']} ${
+              index === Math.abs(position.x - 1)
+                ? `${styles['active']}`
+                : ''
+            }`}
+            style={{
+              transform: `translateY(${offset.y * index}vw)`,
+            }}
+          >
+            <div className={`${styles['phase-wrapper']}`}>
+              <p>{item.phase}</p>
+            </div>
+            <div className={`${styles['timeframe-wrapper']}`}>
+              <p>{item.date}</p>
+            </div>
+            <SliderContent
+              list={item.content}
+              showBenefits={showBenefits}
+              isActive={index === Math.abs(position.x - 1)}
+            />
+            <div></div>
+          </div>
           ))}
       </div>
       <div className={`${styles['arrows-wrapper']}`}>
