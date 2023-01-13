e9cbca6bc420e38678418fce7cd5a4f0a09179e5
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-13 09:30:58 +0200

    added litepaper pdf links to footer

--- components/Generic/SimpleDialog/Dialog.jsx
+++ components/Generic/SimpleDialog/Dialog.jsx
@@ -1,9 +1,9 @@
-import { Dialog, DialogTitle } from '@mui/material';
+import { Dialog as MuiDialog, DialogTitle } from '@mui/material';
 import PropTypes from 'prop-types';
 import useStore from '../../../store/store';
-import styles from './simpledialog.module.scss';
+import styles from './dialog.module.scss';
 
-function SimpleDialog({ onClose, selectedValue, open, title, children }) {
+function Dialog({ onClose, selectedValue, open, title, children }) {
   const handleClose = () => {
     onClose(selectedValue);
   };
@@ -15,22 +15,21 @@ function SimpleDialog({ onClose, selectedValue, open, title, children }) {
   const { isDarkMode } = useStore();
 
   return (
-    <Dialog onClose={handleClose} open={open}>
+    <MuiDialog onClose={handleClose} open={open}>
       <div className={`${styles['content-wrapper']} ${!isDarkMode ? `${styles.light}` : ''}`}>
         <DialogTitle>{title}</DialogTitle>
         <div>
           {children}
-          
         </div>
       </div>
-    </Dialog>
+    </MuiDialog>
   );
 }
 
-SimpleDialog.propTypes = {
+Dialog.propTypes = {
   onClose: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
   selectedValue: PropTypes.string.isRequired,
 };
 
-export default SimpleDialog
+export default Dialog
