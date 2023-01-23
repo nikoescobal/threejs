6ba54fc9b1cc5cc8b0a93d98f5d25882cf8377b5
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-23 13:00:28 +0200

    added language translation to newsletter section

---
+++ components/Home/NewsLetter/content.js
@@ -0,0 +1,17 @@
+const content = {
+  'en': {
+    title: "Don't miss out. Stay updated!",
+    description: 'Subscribe to our newsletter and be the first to get updates on our products and services.',
+    subscribe: 'Subscribe'
+  },
+  'de': {
+    title: 'Verpassen Sie nichts. Bleiben Sie auf dem Laufenden!',
+    description: `Abonnieren Sie unseren Newsletter und 
+      seien Sie unter den Ersten, die Updates zu 
+      unseren Produkten und Dienstleistungen 
+      erhalten.`,
+    subscribe: 'Abonnieren'
+  }
+}
+
+export default content
