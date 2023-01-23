294830798c1f10ccd78c0ffc1fc9a84ac5eb0641
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-23 13:18:06 +0200

    added language translation to card section

--- components/Home/CardSection/CardSection.jsx
+++ components/Home/CardSection/CardSection.jsx
@@ -1,12 +1,5 @@
 import styles from './cardsection.module.scss';
-import MovingIcon from '@mui/icons-material/Moving';
-import Icon1 from '../../../public/icons/card-icon1.svg';
-import Icon2 from '../../../public/icons/card-icon2.svg';
-import Icon3 from '../../../public/icons/card-icon3.svg';
-// import Icon1 from '../../../public/icons/mindfulness.svg';
-// import Icon2 from '../../../public/icons/empathy.svg';
-// import Icon3 from '../../../public/icons/discipline.svg';
-import Icon4 from '../../../public/icons/card-icon4.svg';
+
 import Image from 'next/image';
 import Chests from '../../../public/backgrounds/chests.webp';
 import ChestsFallback from '../../../public/backgrounds/chests.png';
@@ -17,10 +10,14 @@ import Connector3 from '../../../public/illustrations/connector3.svg';
 import Link from 'next/link';
 import ComingSoon from '../../Generic/ComingSoon/ComingSoon';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
+import content from './content';
+import translations from '../../../language';
 
 function CardSection() {
   const { isDarkMode } = useStore();
   const [error, setError] = useState(false);
+  const { locale } = useRouter();
 
   return (
     <div
@@ -52,78 +49,22 @@ function CardSection() {
           alt="connector"
           className={`${styles['connector']}`}
         />
-        <div className={`${styles['card']}`}>
-          <div className={`${styles['icon-wrapper']}`}>
-            <Image src={Icon1} alt="icon" />
-          </div>
-          <div>
-            {/* <h4>Mindfulness</h4> */}
-            <h4>Choose a skill you want to learn</h4>
-
-            {/* <p>
-              Mindfulness is a <strong>key</strong> component of personal
-              development.
-            </p> */}
-            <p>
-              {' '}
-              Our unique education system covers all essential skills for your
-              personal success in mental health, physical health, platonic
-              relationships, romantic relationships, career/finances and much
-              more.
-            </p>
-          </div>
-        </div>
-        <div className={`${styles['card']}`}>
-          <div className={`${styles['icon-wrapper']}`}>
-            <Image src={Icon2} alt="icon" />
-          </div>
-          <div>
-            <h4>Play through lessons and level up your character </h4>
-            <p>
-              Your in-game character is a reflection of you. When the character
-              levels up in-game, you level up in real life. The character allows
-              you to gain an objective perspective of yourself and to keep track
-              of your progress.
-            </p>
-            {/* <h4>Empathy</h4>
-            <p>
-              Life is a reflection of oneself. External conditions{' '}
-              <strong>mirror</strong> internal ones.
-            </p> */}
-          </div>
-        </div>
-        <div className={`${styles['card']}`}>
-          <div className={`${styles['icon-wrapper']}`}>
-            <Image src={Icon3} alt="icon" />
-          </div>
-          <div>
-            <h4>Test and deepen your knowledge </h4>
-            <p>
-              In the beginning, the reward distribution will be pegged to
-              randomized, theoretical exams. The exams are created in a way
-              which tests your deep understanding of the topic, not your memory.
-              As the project develops further, we will implement more
-              opportunities to earn rewards.
-            </p>
-          </div>
-        </div>
-        <div className={`${styles['card']}`}>
-          <div className={`${styles['icon-wrapper']}`}>
-            <Image src={Icon4} alt="icon" />
-          </div>
-          <div>
-            <h4>Earn reward chests </h4>
-            <p>
-              Chests can include Legacy Token (LGCT), collectibles and other
-              items which can either be traded or reused within the ecosystem.
-            </p>
-            {/* <h4>Discipline</h4>
-            <p>
-              Discipline is the way to transform the intangible into{' '}
-              <strong>tangible</strong> .
-            </p> */}
-          </div>
-        </div>
+        {
+          content[locale].map((card) => (
+            <div key={card.title} className={`${styles['card']}`}>
+              <div className={`${styles['icon-wrapper']}`}>
+                <Image src={card.icon} alt="icon" />
+              </div>
+              <div>
+                <h4>{card.title}</h4>
+                <p>
+                  {' '}
+                  {card.description}
+                </p>
+              </div>
+            </div>
+          ))
+        }
       </div>
       <div
         style={{
@@ -138,7 +79,7 @@ function CardSection() {
           <a className='button-blue'>Learn More</a>
         </Link> */}
         <ComingSoon style={{ paddingLeft: '2rem', paddingRight: '2rem' }}>
-          Learn More
+          {translations['learn_more'][locale]}
         </ComingSoon>
       </div>
     </div>
---
+++ components/Home/CardSection/content.js
@@ -0,0 +1,87 @@
+import Icon1 from '../../../public/icons/card-icon1.svg';
+import Icon2 from '../../../public/icons/card-icon2.svg';
+import Icon3 from '../../../public/icons/card-icon3.svg';
+import Icon4 from '../../../public/icons/card-icon4.svg';
+
+const content = {
+  'en': [
+    {
+      title: "Choose a skill you want to learn",
+      description: `Our unique education system covers all essential skills for your
+        personal success in mental health, physical health, platonic
+        relationships, romantic relationships, career/finances and much
+        more.`,
+      icon: Icon1
+    },
+    {
+      title: "Play through lessons and level up your character",
+      description: `Your in-game character is a reflection of you. When the character
+        levels up in-game, you level up in real life. The character allows
+        you to gain an objective perspective of yourself and to keep track
+        of your progress.`,
+      icon: Icon2
+    },
+    {
+      title: "Test and deepen your knowledge",
+      description: `In the beginning, the reward distribution will be pegged to
+        randomized, theoretical exams. The exams are created in a way
+        which tests your deep understanding of the topic, not your memory.
+        As the project develops further, we will implement more
+        opportunities to earn rewards.`,
+      icon: Icon3
+    },
+    {
+      title: "Earn reward chests",
+      description: `Chests can include Legacy Token (LGCT), collectibles and other
+        items which can either be traded or reused within the ecosystem.`,
+      icon: Icon4
+    },
+  ],
+  'de': [
+    {
+      title: "Wählen Sie eine Fähigkeit, die Sie erlernen möchten",
+      description: `Unser einzigartiges Bildungssystem umfasst 
+        alle wichtigen Fähigkeiten für Ihren 
+        persönlichen Erfolg in Bezug auf mentale 
+        Gesundheit, körperliche Gesundheit, 
+        platonische Beziehungen, romantische 
+        Beziehungen, Karriere/Finanzen und vieles 
+        mehr.`,
+      icon: Icon1
+    },
+    {
+      title: "Spielen Sie durch Lektionen und verbessern Sie Ihren Charakter",
+      description: `Ihr In-Game-Charakter ist ein Spiegelbild 
+        von Ihnen. Wenn der Charakter im Spiel 
+        aufsteigt, steigen Sie im echten Leben auf. 
+        Der Charakter ermöglicht es Ihnen, eine 
+        objektive Perspektive auf sich selbst zu 
+        gewinnen und Ihren Fortschritt zu 
+        verfolgen.`,
+      icon: Icon2
+    },
+    {
+      title: "Testen und vertiefen Sie Ihr Wissen",
+      description: `Am Anfang wird die Belohnungsverteilung 
+        an zufällige, theoretische Prüfungen 
+        gekoppelt sein. Die Prüfungen werden so 
+        erstellt, dass sie Ihr tiefes Verständnis des 
+        Themas testen, nicht Ihr Gedächtnis. In 
+        einem späteren Schritt werden wir mehr 
+        Möglichkeiten zur Vergabe von 
+        Belohnungen implementieren.`,
+      icon: Icon3
+    },
+    {
+      title: "Verdienen Sie Belohnungstruhen",
+      description: `Truhen können Legacy Token (LGCT), 
+        Sammlerstücke und andere Gegenstände 
+        enthalten, die entweder gehandelt oder 
+        innerhalb des Ökosystems 
+        wiederverwendet werden können.`,
+      icon: Icon4
+    },
+  ],
+}
+
+export default content;
