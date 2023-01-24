696571b1d946a8c749104aab349336e80586ef68
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-24 10:39:02 +0200

    added translation to About Us body section

---
+++ components/AboutUs/About/content.js
@@ -0,0 +1,195 @@
+const content = {
+  en: [
+    {
+      title: 'About Us',
+      description: `Legacy Network is an international technology company based in
+        Liechtenstein/ Switzerland. Our mission is to improve
+        people&apos;s lives by developing innovative technology and
+        building international communities around personal development. We
+        firmly believe that personal development is the key to success in
+        all life areas, as well as the solution for many global problems.
+        For this reason, we committed ourselves to find new ways in order
+        to provide something of true value to the world.Our central
+        product is a gamified education app, which uses a play-to-earn
+        system to reward its users with crypto. The app is powered by the
+        first ever artificial intelligence, which analyses several aspects
+        of a user&apos;s personality and reflects the collected
+        information back to him.`
+    },
+    {
+      title: 'Our Vision',
+      description: `<span>Our vision</span> is to create the world&apos;s biggest
+      personal development ecosystem with our technology as the gate to
+      a successful and fulfilling life. Great technology should work for
+      the user, not against him. We live in an era, where big tech
+      companies know much more about us and our psychology, than we do.
+      This valuable data is used to sell us more products and to make us
+      neurologically addicted to certain services. We aim to create a
+      technology, which collects the same data but provides it to the
+      user, A technology, developed to give people deep insights into
+      the programming and functionality of their own brain along with
+      individual and playful step by step guidance on how to use the new
+      gained knowledge in their own favor.`
+    },
+    {
+      title: 'The Problem',
+      description: `As things stand today, over 35% of people in Europe suffer from a
+        mental illness that requires treatment. This does not include
+        harmful behaviors and traits that severely limit a person&apos;s
+        quality of life but are not recognized as an illness. The average
+        person today is plagued by: Diverse anxieties, sleep disturbances,
+        recurring negative emotions as well as poor physical condition.
+        These matters in turn negatively impact his success in various key
+        areas of life. We humans tend to perceive certain circumstances as
+        &ldquo;not so bad&rdquo; as far as most of them are concerned. In
+        reality, the facts mentioned have disastrous consequences both for
+        the individuals affected and for society as a whole.
+        <br />
+        Aggression, hatred and criminality within society are largely due
+        to frustration. Frustration, in turn, can be attributed mainly to
+        a lack of success in important areas of life. The main reason for
+        the described problem lies mainly in the human preference for
+        short-term gratification over long-term success, happiness and
+        long-term health as a basic condition. In general, it can be said
+        that the average person of today is trapped in a state of aimless
+        distraction and lacks the necessary strategies and tools to escape
+        from it.`
+    },
+    {
+      title: 'Our Solution',
+      description: `We are developing a logic-respectively science-based play-to-earn
+        system, which explains to the user in a playful way his own
+        behavior as well as that of his fellow people and teaches him step
+        by step effective life strategies. The user learns important
+        skills that supports him actively and independently to create a
+        fulfilling and successful life. The content is mainly from the
+        fields of psychology, sociology, neurology and biology. The
+        contents originate from experts and is subject to strict quality
+        control. Our goal with the play-to-earn system is for the user to
+        not view the learning process as an expense. Even though we have
+        no direct competition in terms of the technology, we still compete
+        with other companies for the user&apos;s attention.
+        <br />
+        A big problem of our time is the neurological dependence on
+        dopamine, or short-term reward release in the brain. By designing
+        our system to create what in this case is a positive dependency,
+        we ensure that our ecosystem is actively used and that the user
+        retains his motivation.`
+    },
+  ],
+  de: [
+    {
+      title: 'Über uns',
+      description: `Legacy Network ist ein internationales 
+        Technologieunternehmen mit Sitz in 
+        Liechtenstein / Schweiz. Unsere Mission ist 
+        es, das Leben der Menschen durch die 
+        Entwicklung innovativer Technologie und 
+        den Aufbau internationaler 
+        Gemeinschaften rund um die persönliche 
+        Entwicklung zu verbessern. Wir glauben 
+        fest daran, dass die persönliche 
+        Entwicklung der Schlüssel zum Erfolg in 
+        allen Lebensbereichen sowie die Lösung für 
+        viele globale Probleme ist. Aus diesem 
+        Grund haben wir uns verpflichtet, neue 
+        Wege zu finden, um der Welt etwas von 
+        wirklichem Wert zu bieten. Unser zentrales 
+        Produkt ist eine gamifizierte Bildungs-App, 
+        die ein Play-to-Earn-System verwendet, um 
+        ihre Benutzer mit Krypto zu belohnen. Die 
+        App wird von der ersten künstlichen 
+        Intelligenz angetrieben, die mehrere 
+        Aspekte der Persönlichkeit eines Benutzers 
+        analysiert und die gesammelten 
+        Informationen zurück an ihn reflektiert.`
+    },
+    {
+      title: 'Unsere Vision',
+      description: `<span>Unsere Vision</span> ist es, mit unserer
+        Technologie das Tor zu einem erfolgreichen
+        und erfüllenden Leben und dem größten
+        Ökosystem für persönliche Entwicklung der
+        Welt zu schaffen. Großartige Technologie
+        sollte für den Benutzer arbeiten und nicht
+        gegen ihn. Wir leben in einer Ära, in der
+        große Technologieunternehmen viel mehr
+        über uns und unsere Psychologie wissen,
+        als wir selbst. Diese wertvollen Daten
+        werden verwendet, um uns mehr Produkte
+        zu verkaufen und uns neurologisch an
+        bestimmte Dienste zu gewöhnen. Wir
+        möchten eine Technologie entwickeln, die
+        die gleichen Daten sammelt, sie dem
+        Benutzer jedoch zur Verfügung stellt. Eine
+        Technologie, die entwickelt wurde, um den
+        Menschen tiefe Einblicke in die
+        Programmierung und Funktionsweise ihres
+        eigenen Gehirns sowie individuelle und
+        spielerische Schritt-für-Schritt-Anleitungen
+        darüber zu geben, wie sie das neu
+        gewonnene Wissen zu ihrem Vorteil nutzen
+        können.`
+    },
+    {
+      title: 'Das Problem',
+      description: `Wie die Dinge heute stehen, leiden über
+        35% der Menschen in Europa an einer
+        psychischen Erkrankung, die behandelt
+        werden muss. Dies schließt schädliche
+        Verhaltensweisen und Eigenschaften aus,
+        die die Lebensqualität einer Person stark
+        einschränken, aber nicht als Krankheit
+        anerkannt werden. Der durchschnittliche
+        Mensch von heute ist geschlagen von:
+        Vielfältigen Ängsten, Schlafstörungen,
+        wiederkehrenden negativen Emotionen
+        sowie einer schlechten körperlichen
+        Verfassung. Diese Angelegenheiten
+        beeinträchtigen auf der anderen Seite
+        seinen Erfolg in verschiedenen wichtigen
+        Bereichen des Lebens. Menschen neigen
+        dazu, bestimmte Umstände als &quot;nicht so
+        schlimm&quot; zu betrachten. In Wirklichkeit
+        haben die genannten Fakten sowohl für die
+        betroffenen Individuen als auch für die
+        Gesellschaft insgesamt verheerende
+        Folgen.`
+    },
+    {
+      title: 'Unsere Lösung',
+      description: `Wir entwickeln ein auf Logik und
+        Wissenschaft basierendes Play to earn
+        System, das dem Benutzer auf spielerische
+        Weise sein eigenes Verhalten sowie das
+        seiner Mitmenschen erklärt und ihm
+        schrittweise effektive Lebensstrategien
+        beibringt. Der Benutzer lernt wichtige
+        Fähigkeiten, die ihn aktiv und unabhängig
+        unterstützen, ein erfülltes und
+        erfolgreiches Leben zu führen. Der Inhalt
+        stammt hauptsächlich aus den Bereichen
+        Psychologie, Soziologie, Neurologie und
+        Biologie. Unser Ziel mit dem Play to earn-
+        System ist es, dass der Benutzer den
+        Lernprozess nicht als Ausgabe betrachtet.
+        Obwohl wir keine direkte Konkurrenz in
+        Bezug auf die Technologie haben,
+        konkurrieren wir trotzdem mit anderen
+        Unternehmen um die Aufmerksamkeit des
+        Benutzers.
+        <br />
+        Ein großes Problem unserer Zeit ist die
+        neurologische Abhängigkeit von Dopamin,
+        also der Freisetzung von kurzfristigen
+        Belohnungen im Gehirn. Indem wir unser
+        System so gestalten, dass es in diesem Fall
+        eine positive Abhängigkeit schafft, stellen
+        wir sicher, dass unser Ökosystem aktiv
+        genutzt wird und der Benutzer seine
+        Motivation behält.`
+    },
+  ]
+}
+
+export default content;
