7a11e7a50b93e4293f53ed14535690326e16b75d
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-25 04:06:19 +0200

    added translation to NFTSection

--- components/TokenPage/NFTSection/NFTSection.jsx
+++ components/TokenPage/NFTSection/NFTSection.jsx
@@ -6,9 +6,12 @@ import Image from 'next/image';
 import CharacterCard from '../CharacterCard/CharacterCard';
 import WaveLines from '../../Generic/WaveLines/WaveLines';
 import VikingOne from '../../../public/nft/viking-big.webp';
+import { useRouter } from 'next/router';
+import content from './content';
 
 function NFTSection() {
   const { isDarkMode } = useStore((state) => state);
+  const { locale } = useRouter();
 
   return (
     <div
@@ -18,14 +21,9 @@ function NFTSection() {
     >
       <WaveLines className={styles.waves} />
       <div className={`${styles['text-wrapper']} max-1920`}>
-        <h3>Discover Our NFT</h3>
+        <h3>{content[locale].title}</h3>
         <p>
-          The Legacy Network NFT is an in-game character with a unique set of
-          features. There are 5 different tribes that hold the legacy. They come
-          in different forms of animation, face props, armour, colour, and
-          character traits. Each trait such as character, animation, face prop,
-          etc. also has multiple tiers of rarity, resulting in unique and epic
-          combinations for you to own!
+          {content[locale].description_1}
         </p>
       </div>
       <div className={`${styles['first-card-section']} max-1920`}>
@@ -90,9 +88,7 @@ function NFTSection() {
           </CharacterCard>
         </div>
         <p>
-          The Legacy Network NFT on Ethereum is the first batch of characters
-          that will populate the Legacy Network play-to-earn game app, earning
-          these NFT an armour or upgrades indicate their elite status.
+          {content[locale].description_2}
         </p>
       </div>
     </div>
