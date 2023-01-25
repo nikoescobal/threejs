1d095792e3c6ecc8662c23958fc2c79baa7b17ef
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-25 02:40:34 +0200

    added translation to BlockchainBanner section

---
+++ components/TokenPage/BlockchainBanner/content.js
@@ -0,0 +1,34 @@
+const content = {
+  en: {
+    title: 'Blockchain',
+    description: `The Legacy Token will be available on multiple blockchains.
+      VeChain as native blockchain, as well as Binance smart chain,
+      Ethereum and Polygon. The goal of multiple blockchains is to
+      address a target group as broad as possible, and to use the
+      individual advantages of the respective blockchains. The
+      circulation supply of the respective tokens is balanced by means
+      of atomic swap, so that the statistics of the tokens cannot change
+      independently of each other. For transferring tokens between the
+      different blockchains, the new cross-chain bridge of safeswap is
+      used.`
+  },
+  de: {
+    title: 'Blockchain',
+    description: `Der Legacy Token wird auf mehreren Blockchains 
+      verfügbar sein. VeChain als native Blockchain 
+      sowie Binance Smart Chain, Ethereum und 
+      Polygon. Das Ziel von mehreren Blockchains ist 
+      es, eine möglichst breite Zielgruppe 
+      anzusprechen und die individuellen Vorteile der 
+      jeweiligen Blockchains zu nutzen. Das 
+      Umlaufangebot der jeweiligen Tokens wird durch 
+      Atomic Swap ausgeglichen, sodass die Statistiken 
+      der Tokens sich nicht unabhängig voneinander 
+      verändern können. Für die Übertragung von 
+      Tokens zwischen den verschiedenen Blockchains 
+      wird die neue Cross-Chain-Bridge von Safeswap 
+      verwendet.`
+  }
+}
+
+export default content;
