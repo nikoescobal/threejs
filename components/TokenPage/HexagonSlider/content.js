1fb27b96f4fdc09ffe0ec646f4a6d689e5c32186
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-25 02:28:21 +0200

    added translation to HexagonSlider section

---
+++ components/TokenPage/HexagonSlider/content.js
@@ -0,0 +1,87 @@
+const content = {
+  en: [
+    {
+      title: 'Get rewarded for learning',
+      body: 'Our goal is to make education as playful and entertaining as possible. Many people struggle with building the necessary discipline to educate themselves on a regular basis. The main reason for this problematic is found in neurology. As things stand today, most people are neurologically addicted to instant gratification, short-termed dopamine release within the brain. We aim to take advantage of this effect and to use it for the user instead of using it against him',
+    },
+    {
+      title: 'Get access to our products and services',
+      body: 'The Legacy Token (LGCT) serves as the main payment method for subscriptions and purchases within our ecosystem. This allows us to create automatisms such as redistributions and to act independently of third-party providers.',
+    },
+    {
+      title: 'Stake your Legacy Token (LGCT) for additional benefits',
+      body: 'Staking your Legacy Token (LGCT) will grant you additional benefits such as staking rewards, airdrops, whitelist spots, discounts and access to exclusive services. ',
+    },
+    {
+      title: 'Business to business utility',
+      body: 'We plan to make selected parts of our ecosystem such as content libraries and code accessible for other businesses in future. The Legacy Token (LGCT) will be needed to access our B2B services which will benefit the community.',
+    },
+    {
+      title: 'Get rewarded for contributing to our ecosystem',
+      body: 'In future, users will be able to perform certain actions within the social part of our ecosystem and to earn rewards for doing so.',
+    },
+    {
+      title: 'More about to come',
+      body: `We haven't even started yet! Our goal is to create as much value for our community as possible. For this reason, new, exciting features will be announced on a regular basis.`,
+    },
+  ],
+  de: [
+    {
+      title: 'Werde belohnt fürs lernen',
+      body: `Unser Ziel ist es, die Bildung so spielerisch und 
+      unterhaltsam wie möglich zu gestalten. Viele 
+      Menschen haben Schwierigkeiten, die notwendige 
+      Disziplin aufzubringen, um sich regelmäßig 
+      weiterzubilden. Der Hauptgrund dafür liegt in der 
+      Neurologie. Wie die Dinge heute stehen, sind die 
+      meisten Menschen neurologisch süchtig nach 
+      sofortiger Befriedigung und kurzfristiger Dopamin-
+      Freisetzung im Gehirn. Unser Ziel ist es, diesen Effekt 
+      zu bestmöglich zu nutzen und ihn für den Benutzer zur 
+      Verfügung zu stellen, anstatt ihn gegen ihn zu 
+      verwenden.`,
+    },
+    {
+      title: 'Erhalte Zugang zu unseren Produkten und Dienstleistungen',
+      body: `Mit dem Legacy Token (LGCT) erhalten Sie Zugang zu 
+        unseren Produkten und Dienstleistungen. Der LGCT 
+        dient als Hauptzahlungsmethode für Abonnements 
+        und Einkäufe in unserem Ökosystem. Dadurch können 
+        wir Automatismen wie Umverteilungen schaffen und 
+        unabhängig von Drittanbietern handeln.`,
+    },
+    {
+      title: 'Staken Sie Ihren Legacy Token (LGCT), um zusätzliche Vorteile zu erhalten',
+      body: `Legen Sie Ihren Legacy Token (LGCT) an, um 
+        zusätzliche Vorteile zu erhalten. Das Anlegen Ihres 
+        Legacy Tokens (LGCT) gewährt Ihnen zusätzliche 
+        Vorteile wie Anreize für das Anlegen, Airdrops, Plätze 
+        auf der Whitelist, Rabatte und Zugang zu exklusiven 
+        Dienstleistungen.`,
+    },
+    {
+      title: 'Business-to-Business-Nutzwert',
+      body: `Wir planen, in Zukunft ausgewählte Teile unseres 
+        Ökosystems wie Inhaltsbibliotheken und Code für 
+        andere Unternehmen zugänglich zu machen. Der 
+        Legacy Token (LGCT) wird erforderlich sein, um auf 
+        unsere B2B-Dienste zugreifen zu können, wovon die 
+        Community profitieren wird.`,
+    },
+    {
+      title: 'Erhalte Belohnungen für deinen Beitrag zu unserem Ökosystem',
+      body: `In Zukunft werden Nutzer in der Lage sein, bestimmte 
+      Aktionen im sozialen Teil unseres Ökosystems 
+      auszuführen und Belohnungen dafür zu erhalten.`,
+    },
+    {
+      title: 'Es folgt noch mehr!',
+      body: `Wir haben noch gar nicht angefangen! Unser Ziel ist 
+      es, unserer Community so viel Wert wie möglich zu 
+      bieten. Aus diesem Grund werden regelmäßig neue 
+      und aufregende Funktionen bekannt gegeben werden.`,
+    },
+  ],
+}
+
+export default content;
