fb4e65bd3284fbbabded9d886a0ab6fd773a27d2
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-26 19:20:19 +0200

    added translations to Benefits section

---
+++ components/App/Benefits/content.js
@@ -0,0 +1,100 @@
+const content = {
+  en: {
+    title: 'Your Benefits',
+    description: 'With a lot of powerful features, we guarantee simplity and clarity',
+    benefits: [
+      {
+        title: 'Education',
+        icon: '/icons/play.svg',
+        alt: 'play icon',
+        list: [
+          'Get access to high quality content for your personal growth',
+          'Keep up your motivation thanks to our play to earn system',
+          'Profit from a fair and flexible subscription model',
+          'Test and deepen your new gained knowledge'
+        ]
+      },
+      {
+        title: 'Community',
+        icon: '/icons/people.svg',
+        alt: 'people icon',
+        list: [
+          'Connect with like-minded people who are on the same path',
+          'Exchange knowledge and ressources with your peers',
+          'Find your twins and build your own legacy',
+          'Participate in exciting events'
+        ]
+      },
+      {
+        title: 'Execution',
+        icon: '/icons/notebook.svg',
+        alt: 'notebook icon',
+        list: [
+          'Receive individual quests and apply your learnings right away',
+          'Understand and fix what holds you back from taking action',
+          'Increase your capability and exceed your limits step by step',
+        ]
+      },
+      {
+        title: 'Monitoring',
+        icon: '/icons/charts.svg',
+        alt: 'charts icon',
+        list: [
+          'Notice problems early and make the necessary adjustments',
+          'Enjoy to see your growth and the growth of your character',
+          'Keep track of your progress in the statistics area',
+        ]
+      },
+    ]
+  },
+  de: {
+    title: 'Ihre Vorteile',
+    description: 'Mit vielen leistungsstarken Funktionen garantieren wir Einfachheit und Klarheit',
+    benefits: [
+      {
+        title: 'Bildung',
+        icon: '/icons/play.svg',
+        alt: 'play icon',
+        list: [
+          'Erhalten Sie Zugang zu hochwertigen Inhalten für Ihre persönliche Entwicklung',
+          'Halten Sie Ihre Motivation durch unser Play-to-Earn-System aufrecht',
+          'Profitieren Sie von einem fairen und flexiblen Abonnementmodell',
+          'Testen und vertiefen Sie Ihr neu erworbenes Wissen'
+        ]
+      },
+      {
+        title: 'Community',
+        icon: '/icons/people.svg',
+        alt: 'people icon',
+        list: [
+          'Verbinden Sie sich mit Gleichgesinnten, die auf dem gleichen Weg sind',
+          'Tauschen Sie Wissen und Ressourcen mit Ihren Peers aus',
+          'Finden Sie Ihre Zwillinge und bauen Sie Ihr eigenes Erbe auf',
+          'Nehmen Sie an aufregenden Veranstaltungen teil'
+        ]
+      },
+      {
+        title: 'Ausführung',
+        icon: '/icons/notebook.svg',
+        alt: 'notebook icon',
+        list: [
+          'Erhalten Sie individuelle Aufgaben und setzen Sie Ihr gelerntes Wissen sofort um',
+          'Verstehen und beheben Sie, was Sie davon abhält, Taten zu setzen',
+          'Steigern Sie Ihre Fähigkeiten und überschreiten Sie Ihre Grenzen Schritt für Schritt',
+        ]
+      },
+      {
+        title: 'Überwachung',
+        icon: '/icons/charts.svg',
+        alt: 'charts icon',
+        list: [
+          'Beachten Sie Probleme frühzeitig und treffen Sie die notwendigen Anpassungen',
+          'Freuen Sie sich, Ihren Fortschritt und den Fortschritt Ihrer Persönlichkeit zu sehen',
+          'Verfolgen Sie Ihren Fortschritt im Statistikbereich',
+        ]
+      },
+    ]
+  }
+}
+
+export default content;
