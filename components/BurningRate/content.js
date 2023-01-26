833940168829f0f1a8fa192ae25e93436afc562f
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-26 16:01:08 +0200

    added translations to BurningRate component

--- components/BurningRate/content.js
+++ components/BurningRate/content.js
@@ -1,5 +1,7 @@
 const content = {
-  en: [
+  en: {
+    title: '',
+    list: [
     {
       title: 'Development',
       content:
@@ -25,36 +27,37 @@ const content = {
       content:
         'A big share of the Legacy Token (LGCT)s which are being paid for in-app subscriptions get automatically burned which makes Legacy Token (LGCT) deflationary.',
     },
-  ],
-  de: [
+  ]},
+  de: {
+    title: 'Ein Multi Chain Utility & Zahlungstoken',
+    list: [
     {
-      title: 'Development',
+      title: 'Entwicklung',
       content:
-        'Backflow of Legacy Token (LGCT)s into a company owned wallet to cover running costs and to grant further development and expansion of the Legacy Network ecosystem.',
+        'Rückfluss von Legacy Token (LGCT) in eine von der Firma besitzte Wallet, um laufende Kosten zu decken und weitere Entwicklung und Erweiterung des Legacy Network-Ökosystems zu ermöglichen.',
     },
     {
       title: 'Staking-Pool',
       content:
-        `Genau wie der Play-to-Earn-Pool benötigt auch der 
-        Staking-Pool Liquidität, die von bestehenden 
-        Nutzern bereitgestellt wird.`,
+        `Genau wie der Play-to-Earn-Pool benötigt auch der Staking-Pool Liquidität, die von bestehenden Nutzern bereitgestellt wird.
+        `,
     },
     {
-      title: 'P2E Pool',
+      title: 'P2E-Pool',
       content:
-        'Backflow of Legacy Token (LGCT)s into the play to earn pool to make sure that it always has enough liquidity.',
+        'Rückfluss von Legacy Token (LGCT) in den Play-to-Earn-Pool, um sicherzustellen, dass immer genügend Liquidität vorhanden ist.',
     },
     {
-      title: 'NFT holders',
+      title: 'NFT-Inhaber',
       content:
-        'Distribution of Legacy Token (LGCT)s to NFT holders as an additional NFT utility.',
+        'Verteilung von Legacy Token (LGCT) an NFT-Inhaber als zusätzliche NFT-Nutzung.',
     },
     {
-      title: 'Burn',
+      title: 'Verbrennen',
       content:
-        'A big share of the Legacy Token (LGCT)s which are being paid for in-app subscriptions get automatically burned which makes Legacy Token (LGCT) deflationary.',
+        'Ein großer Teil der Legacy Token (LGCT), die für In-App-Abonnements bezahlt werden, werden automatisch verbrannt, was den Legacy Token (LGCT) deflationär macht.',
     },
-  ]
+  ]}
 }
 
 export default content;
