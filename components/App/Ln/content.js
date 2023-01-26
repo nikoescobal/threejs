614143263354396d7f69c71e8e47982baf18566b
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-01-26 19:56:46 +0200

    added translations to LN section

---
+++ components/App/Ln/content.js
@@ -0,0 +1,138 @@
+const content = {
+  en: {
+    title: 'Legacy Wallet',
+    tabs: [
+      'Managing',
+      'Staking',
+      'Vesting',
+      'Bridging'
+    ],
+    panels: [
+      {
+        content: `
+          <h4>Managing</h4>
+          <p>
+            Manage your favorite cryptos in a secure non-custodial wallet.
+            The legacy wallet allows you to easily hold, send and receive
+            tokens on multiple blockchains. The private keys are stored
+            locally on the device so you and only you have access to your
+            funds. Browse through the NFT section to see all your
+            collectibles safe in one place. Additionally, the wallet comes
+            with a browser extension which grants you web-access to the
+            exciting world of Web3.
+          </p>
+          <ul>
+            Token and NFT support:
+            <li>VeChain</li>
+            <li>Ethereum</li>
+            <li>Binance</li>
+            <li>Polygon.</li>
+          </ul>
+        `,
+        img: '/mockups/managing.png',
+      },
+      {
+        content: `
+          <h4>Staking</h4>
+          <p>
+            As a long-term believer, you can stake your Legacy Token (LGCT)s
+            for 6, 12, or 24 months to benefit from exclusive rewards.
+            Alternatively, the flexible staking option can be used, so you
+            can withdraw your funds at any time. Our flexible staking option
+            comes with an auto-compounding feature which saves you time by
+            automatically claiming the rewards and by re-staking them. The
+            tokens are held by audited smart contracts to grant high
+            security and performance.
+          </p>
+        `,
+        img: '/mockups/staking.png',
+      },
+      {
+        content: `
+          <h4>Vesting</h4>
+          <p>
+            As a private sale, public sale or giveaway participant, you can
+            claim your vested Legacy Token (LGCT)s directly in the wallet.
+            The feature calculates the claimable amount live, so tokens can
+            be claimed in an interval of your choice. Just like the staking
+            contracts, the vesting contracts are audited as well which
+            grants high security and performance.
+          </p>
+        `,
+        img: '/mockups/vesting.png',
+      },
+      {
+        content: `
+          <h4>Bridging</h4>
+          <p>
+            Bridge your Legacy Token (LGCT)s between the supported
+            blockchains in an instant thanks to our SafeSwap integration.
+            Atomic swaps are used which makes bridging cheap, fast and
+            secure. More than that, we are working on an auto-bridge feature
+            which allows you to send your Legacy Token (LGCT)s from one
+            blockchain to another without needing to bridge them first.
+          </p>
+        `,
+        img: '/mockups/bridging.png',
+      },
+    ]
+  },
+  de: {
+    title: 'Legacy Wallet',
+    tabs: [
+      'Managing',
+      'Staking',
+      'Vesting',
+      'Bridging'
+    ],
+    panels: [
+      {
+        content: `
+          <h4>Verwaltung</h4>
+          <p>
+          Verwalten Sie Ihre bevorzugten Kryptowährungen in einer sicheren, non-custodialen Wallet. Die Legacy-Wallet ermöglicht es Ihnen, Token auf mehreren Blockchains einfach zu halten, zu senden und zu empfangen. Die privaten Schlüssel werden lokal auf dem Gerät gespeichert, sodass nur Sie Zugriff auf Ihre Mittel haben. Durchsuchen Sie die NFT-Sektion, um alle Ihre Sammlerstücke an einem sicheren Ort zu sehen. Darüber hinaus verfügt die Wallet über eine Browser-Erweiterung, mit der Sie Zugriff auf die aufregende Welt von Web3 haben.
+          </p>
+          <ul>
+            Token und NFT support:
+            <li>VeChain</li>
+            <li>Ethereum</li>
+            <li>Binance</li>
+            <li>Polygon.</li>
+          </ul>
+        `,
+        img: '/mockups/managing.png',
+      },
+      {
+        content: `
+          <h4>Staking</h4>
+          <p>
+          Als langfristiges Mitglied können Sie Ihre Legacy Token (LGCT)s für 6, 12 oder 24 Monate hinterlegen, um von exklusiven Belohnungen zu profitieren. Alternativ kann die flexible Staking-Option verwendet werden, sodass Sie Ihre Mittel jederzeit abheben können. Die flexible Staking-Option verfügt über eine Auto-Compoundfunktion, die Ihnen Zeit spart, indem sie automatisch die Belohnungen beansprucht und erneut beschlagnahmt. Die Tokens werden von überprüften Smart Contracts gehalten, um eine hohe Sicherheit und Leistung zu gewährleisten.
+
+          </p>
+        `,
+        img: '/mockups/staking.png',
+      },
+      {
+        content: `
+          <h4>Vesting</h4>
+          <p>
+          Als Teilnehmer eines privaten Verkaufs, öffentlichen Verkaufs oder Giveaways können Sie Ihre gesicherten Legacy Token (LGCT)s direkt in der Wallet beanspruchen. Die Funktion berechnet den beanspruchbaren Betrag in Echtzeit, sodass Token in einem Intervall Ihrer Wahl beansprucht werden können. Genau wie die Staking-Verträge werden auch die Vesting-Verträge überprüft, was eine hohe Sicherheit und Leistung gewährleistet.
+
+          </p>
+        `,
+        img: '/mockups/vesting.png',
+      },
+      {
+        content: `
+          <h4>Bridging</h4>
+          <p>
+          Bridgen Sie Ihre Legacy Token (LGCT)s zwischen den unterstützten Blockchains in einem Augenblick dank unserer SafeSwap-Integration. Atomic Swaps werden verwendet, was das Bridging billig, schnell und sicher macht. Darüber hinaus arbeiten wir an einer Auto-Bridge-Funktion, mit der Sie Ihre Legacy Token (LGCT)s von einer Blockchain auf eine andere senden können, ohne sie zuvor verbinden zu müssen.
+          </p>
+        `,
+        img: '/mockups/bridging.png',
+      },
+    ]
+  }
+}
+
+export default content;
