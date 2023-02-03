1d496c24a59f44135fab362c5a92c01d97be3742
Nikolas Escobal <niko.escobal@gmail.com>
2023-02-03 16:27:00 +0800

    Fixed content page for utility section

--- components/TokenPage/TokenIntro/content.js
+++ components/TokenPage/TokenIntro/content.js
@@ -23,9 +23,27 @@ const content = {
     ],
   },
   de: {
-    title: 'Was ist LGCT (LGCT)?',
-    subtitle:
-      'Legacy Token (LGCT) ist ein Multi-Chain Utility und Zahlungstoken, der es den Nutzern erlaubt, das Legacy Network bestmöglich zu nutzen.',
+    title: 'What is Legacy Token (LGCT)?',
+    subtitle: `Legacy Token (LGCT) is a multichain utility and payment token which
+    allows users to get the most out of the Legacy Network ecosystem.`,
+    content: [
+      {
+        heading: 'Name',
+        text: 'Legacy Token',
+      },
+      {
+        heading: 'Blockchain',
+        text: 'Polygon | Vechain | Ethereum | Binance Smart Chain',
+      },
+      {
+        heading: 'Symbol',
+        text: 'LGCT',
+      },
+      {
+        heading: 'Max Supply',
+        text: '720,000,000',
+      },
+    ],
   },
 };
 
--- components/TokenPage/UtilitySectionNew/content.js
+++ components/TokenPage/UtilitySectionNew/content.js
@@ -44,51 +44,46 @@ const content = {
     ],
   },
   de: {
-    title: 'Ihre Vorteile',
-    description:
-      'Mit vielen leistungsstarken Funktionen garantieren wir Einfachheit und Klarheit',
-    benefits: [
+    title: 'Token Utility',
+    // description:
+    //   'With a lot of powerful features, we guarantee simplity and clarity',
+    utilities: [
       {
-        title: 'Bildung',
-        icon: '/icons/play.svg',
-        alt: 'play icon',
-        list: [
-          'Erhalten Sie Zugang zu hochwertigen Inhalten für Ihre persönliche Entwicklung',
-          'Halten Sie Ihre Motivation durch unser Play-to-Earn-System aufrecht',
-          'Profitieren Sie von einem fairen und flexiblen Abonnementmodell',
-          'Testen und vertiefen Sie Ihr neu erworbenes Wissen',
-        ],
+        title: 'Business to business utility',
+        icon: '/icons/education.svg',
+        alt: 'lock icon',
+        text: 'The Legacy Token (LGCT) will be needed to access our B2B services which will benefit the community.',
       },
       {
-        title: 'Community',
-        icon: '/icons/people.svg',
-        alt: 'people icon',
-        list: [
-          'Verbinden Sie sich mit Gleichgesinnten, die auf dem gleichen Weg sind',
-          'Tauschen Sie Wissen und Ressourcen mit Ihren Peers aus',
-          'Finden Sie Ihre Zwillinge und bauen Sie Ihr eigenes Erbe auf',
-          'Nehmen Sie an aufregenden Veranstaltungen teil',
-        ],
-      },
-      {
-        title: 'Ausführung',
-        icon: '/icons/notebook.svg',
+        title: 'Contributing to our ecosystem',
+        icon: '/icons/execution.svg',
         alt: 'notebook icon',
-        list: [
-          'Erhalten Sie individuelle Aufgaben und setzen Sie Ihr gelerntes Wissen sofort um',
-          'Verstehen und beheben Sie, was Sie davon abhält, Taten zu setzen',
-          'Steigern Sie Ihre Fähigkeiten und überschreiten Sie Ihre Grenzen Schritt für Schritt',
-        ],
+        text: 'In future, users will be able to perform certain actions within the social part of our ecosystem and to earn rewards for doing so.',
+      },
+
+      {
+        title: 'Get rewarded for learning',
+        icon: '/icons/dollar.svg',
+        alt: 'dollar icon',
+        text: 'Our goal is to make education as playful and entertaining as possible. We aim to take advantage of a gamified approach to learning.',
       },
       {
-        title: 'Überwachung',
-        icon: '/icons/charts.svg',
-        alt: 'charts icon',
-        list: [
-          'Beachten Sie Probleme frühzeitig und treffen Sie die notwendigen Anpassungen',
-          'Freuen Sie sich, Ihren Fortschritt und den Fortschritt Ihrer Persönlichkeit zu sehen',
-          'Verfolgen Sie Ihren Fortschritt im Statistikbereich',
-        ],
+        title: 'Access to products and services',
+        icon: '/icons/community.svg',
+        alt: 'people icon',
+        text: 'The Legacy Token (LGCT) serves as the main payment method for subscriptions and purchases within our ecosystem. This allows us to create automatisms such as redistributions and to act independently of third-party providers.',
+      },
+      {
+        title: 'Stake your Legacy Token (LGCT) for additional benefits',
+        icon: '/icons/community.svg',
+        alt: 'people icon',
+        text: 'Staking your Legacy Token (LGCT) will grant you additional benefits such as staking rewards, airdrops, whitelist spots, discounts and access to exclusive services.',
+      },
+      {
+        title: 'Stake your Legacy Token (LGCT) for additional benefits',
+        icon: '/icons/box.svg',
+        alt: 'box icon',
+        text: "We haven't even started yet! Our goal is to create as much value for our community as possible. For this reason, new, exciting features will be announced on a regular basis.",
       },
     ],
   },
