a6581f6588180166ef3e9148cdddc33fe409b170
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-02-03 16:11:50 +0200

    created network page header

---
+++ pages/network.js
@@ -0,0 +1,65 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import GenericHeader from '../components/Generic/Header/GenericHeader';
+import WavesWrapper from '../components/Generic/WavesWrapper/WavesWrapper';
+import Explanation from '../components/PersonalDevelopment/Explanation/Explanation';
+import HealthCrisis from '../components/PersonalDevelopment/HealthCrisis/HealthCrisis';
+import StepsForPersonalDevelopment from '../components/PersonalDevelopment/StepsForPersonalDevelopment/StepsForPersonalDevelopment';
+import Achievements from '../public/illustrations/Achievements.png';
+import Waves from '../public/backgrounds/waves-5.png';
+import WaveLines from '../components/Generic/WaveLines/WaveLines';
+import Script from 'next/script';
+import styles from '../components/NetworkPage/Network.module.scss';
+import { useRouter } from 'next/router';
+import WhatIsPersonalDevelopment from '../components/PersonalDevelopment/WhatIsPersonalDevelopment/WhatIsPersonalDevelopment';
+import LegacyMethods from '../components/Home/LegacyMethods/LegacyMethodsList/LegacyMethods';
+import PillarsOfAnUnhappyLife from '../components/PersonalDevelopment/PillarsOfAnUnhappyLife/PillarsOfAnUnhappyLife';
+
+const content = {
+  en: {
+    title: 'LN Network',
+    subtitle: '& its importance'
+  },
+  de: {
+    title: 'Das Netzwerk',
+    subtitle: 'Sinnvolle Veränderung'
+  }
+}
+
+function PersonalDevelopment() {
+  const { locale } = useRouter();
+  
+  return (
+    <div>
+      <Head>
+        <title>Personal Development</title>
+        <meta name="description" content="Personal Development" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <GenericHeader
+        waves={<WaveLines className={`${styles['waves-wrapper']}`} />}
+        rightObj={
+          <img
+            className={styles.image}
+            src={'/headers/network.png'}
+            alt="network illustration"
+          />
+        }
+        title={content[locale].title}
+        subtitle={content[locale].subtitle}
+      />
+      {/* <Explanation /> */}
+      <WhatIsPersonalDevelopment />
+      <LegacyMethods />
+      {/* <WavesWrapper hideBottom={true}>
+      </WavesWrapper> */}
+      <HealthCrisis />
+      <PillarsOfAnUnhappyLife />
+      {/* <StepsForPersonalDevelopment /> */}
+    </div>
+  );
+}
+
+export default PersonalDevelopment;
