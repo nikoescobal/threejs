4f41f104430a0184383278a443e168403b674a8a
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-02-08 00:29:16 +0200

    fixed failed deployment

--- components/TokenPage/DiscoverOurNFT/content.js
+++ components/TokenPage/DiscoverOurNFT/content.js
@@ -69,7 +69,28 @@ const content = {
         text: 'Legendary NFTs',
         amount: 'TBA'
       },
-      
+      info: [
+        {
+          title: 'The African',
+          description: `Ancient African tribes were known for their many diverse cultures, customs, and languages. They developed various forms of art, architecture, and spiritual practices that have been passed down from generation to generation. Additionally, ancient African tribes had complex relationships with their environment and relied heavily on subsistence farming and herding for sustenance.`
+        },
+        {
+          title: 'The Aztec',
+          description: `They were renowned for their sophisticated culture and their remarkable achievements in architecture, art, and astronomy. They also had a complex social structure and a powerful military. The Aztecs were known for their religious rituals, including human sacrifice and the worship of multiple gods.`
+        },
+        {
+          title: 'The Egyptian',
+          description: `The ancient Egyptians were a deeply spiritual people, and their beliefs were a major part of their culture. Egyptians believed that their gods and goddesses controlled the world and that they could intervene in everyday life. They believed that the gods were responsible for the natural processes of the world.`
+        },
+        {
+          title: 'The Greek',
+          description: `Ancient Greek heroes were brave, persevering, and able to adapt to any intense situation. In addition, Greek philosophers had a great influence on today's society, science, and politics. Valuing logic-based knowledge, rational observation, and reason and inquiry.`
+        },
+        {
+          title: 'The Viking',
+          description: `Vikings are Norse warriors best known for their relentless warfare. What many do not know is that they were also peaceful traders, craftsmen, and farmers. Vikings embodied strength and an appreciation for life like no other, and they believed in the teachings of Norse mythology.`
+        }
+      ]
   },
 };
 
