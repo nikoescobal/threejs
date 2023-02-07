3118169844f1d7c5b7b3c62480b65c0733ad95d8
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-02-07 11:49:54 +0200

    added German content

--- components/LegacyPage/LegacyMethods/LegacyMethodsList/content.js
+++ components/LegacyPage/LegacyMethods/LegacyMethodsList/content.js
@@ -56,39 +56,59 @@ const content = {
   },
   de: {
     title: 'The Process',
-    subtitle: 'Wir verfolgen einen systematischen Ansatz, der auf <strong>6 Kernmethoden</strong> basiert.',
+    subtitle: 'Users are guided through a structured process that helps them build the routine & discipline required to achieve their goals',
     list: [
-    {
-      title: 'Verstehen Sie die Grundlagen der Persönlichen Entwicklung.',
-      description:
-        'Verstehen Sie jeden Pfeiler und was sie bedeuten. Verstehen Sie den Gesamtraum der PE.',
-    },
-    {
-      title: 'Verstehen Sie sich selbst.',
-      description:
-        'Verstehen Sie Ihren Geist. Was sind Ihre Stärken und Schwächen? Was sind Ihre Ziele? Was ist Ihr Traum/Ihre Vision?',
-    },
-    {
-      title: 'Handeln Sie.',
-      description: 'Erstellen Sie Ihren Plan. Schreiben Sie auf, was wichtig ist. Setzen Sie Ihre Ziele fest.',
-    },
-    {
-      title: 'Erstellen Sie eine Routine.',
-      description:
-        'Teilen Sie Aufgaben in kleinere Teile auf. Seien Sie vorbereitet. Legen Sie zufällige Belohnungen fest.',
-    },
-    {
-      title: 'Aufbauen von Disziplin.',
-      description:
-        "Führen Sie täglich ein Tagebuch. Folgen Sie einem strikten Zeitplan/Aufgabenliste. Denken Sie nicht, handeln Sie einfach.",
-    },
-    {
-      title: 'Lernen Sie niemals aufzuhören.',
-      description:
-        'Aufbauen von Wissen wie Schichten. Halten Sie einen offenen Geist. Lernen Sie jeden Tag eine neue Sache.',
-    },
+      {
+        title: 'Understand the basics of PD.',
+        list: [
+          'Understand each pillar & what they mean',
+          'Learn what’s currently taught',
+          'Understand the overall PD space',
+        ]
+      },
+      {
+        title: 'Understand yourself',
+        list: [
+          'Understand your mind',
+          'What are your strengths & weaknesses ',
+          'What are your goals',
+          'What is your dream/vision',
+        ]
+      },
+      {
+        title: 'Take action',
+        list: [
+          'Create your plan',
+          'Write down what matters ',
+          'Set your goals',
+        ]
+      },
+      {
+        title: 'Create a routine',
+        list: [
+          'Break tasks into smaller pieces',
+          'Be prepared',
+          'Randomised rewards',
+        ]
+      },
+      {
+        title: 'Build discipline',
+        list: [
+          'Journaling',
+          'Strict agenda / ToDo list',
+          'Don’t think, just do',
+        ]
+      },
+      {
+        title: 'Never stop learning',
+        list: [
+          'Build knowledge like layers',
+          'Keep an open mind',
+          'Learning 1 new thing each day',
+        ]
+      },
     ]
-}
+  },
 };
 
 export default content;
