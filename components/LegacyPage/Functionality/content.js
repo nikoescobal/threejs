51b713ea4e1b7310e0f3d87ab938d39a547eca5f
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-02-07 09:54:33 +0200

    added German content for Functionality component

--- components/LegacyPage/Functionality/content.js
+++ components/LegacyPage/Functionality/content.js
@@ -44,25 +44,46 @@ const content = {
     ],
   },
   de: {
-    title:
-      'Die Legacy Network App beantwortet dem Nutzer drei grundlegende Fragen:',
-    mental_health: 'Was ist eine psychische Erkrankung?',
-    mental_description:
-      'Eine psychische Erkrankung ist jede Situation, in der die Handlungen, Gefühle und Verhaltensweisen einer Person dazu führen können, dass sie sich selbst oder andere verletzen, und/oder sie dazu bringen können, sich nicht selbst versorgen oder in der Gemeinschaft auf gesunde Weise funktionieren zu können.',
-    more: 'Mehr zur psychischen Gesundheitskriese',
     list: [
       {
-        title: 'WAS',
-        text: `Hier wird dem Nutzer eine Tatsache mitgeteilt, zum Beispiel, dass ein gesundes Selbstbewusstsein für den Erfolg in verschiedenen Lebensbereichen wichtig ist. Es wird gezeigt, in welchen Lebensbereichen die Eigenschaft Auswirkungen hat.`,
-      },
-      {
-        title: 'WARUM',
-        text: `Im Anschluss daran wird ausführlich erklärt, warum ein gesundes Selbstbewusstsein wichtig ist. Verständliche Zusammenhänge werden gezeigt, um ein "Aha-Erlebnis" zu schaffen.`,
-      },
-      {
-        title: 'WIE',
-        text: `Im letzten Schritt wird erklärt, wie genau Selbstbewusstsein entsteht. Klare Ursache-Wirkungs-Beziehungen werden gezeigt, damit der Nutzer ein tiefes Verständnis für die Eigenschaft erlangt.
+        title: 'Functionality',
+        body: `
+          <p>
+            In the education area of the Legacy App, the user can choose from various categories.
+          </p>
+          <br />
+          <p>
+            Each category contains content, which can be text, video, animation, podcast (audio content) and other forms. Each category has its own level system.
+          </p>
+          <br />
+          <p>
+            Our AI uses targeted questions, user behavior, feedback, app tracking and other parameters to decide how advanced the user already is and which content is relevant to him.
+          </p>
+          <br />
+          <p>
+            This makes the system usable for everyone, whether beginner or professional. The content consists of both a theoretical and a practical part. A topic is explained to the user in an understandable and detailed manner, after which the AI suggests concrete strategies and goals for implementation. The goals are added to the user account and the user is regularly reminded of them. If he does not achieve the goals, the AI suggests suitable alternatives. Rewards in the form of tokens and experience points are distributed for passing a level.
+          </p>
         `,
+        img: {
+          src: '/mockups/Legacy/1.png',
+          alt: 'Legacy App Mockup'
+        }
+      },
+      {
+        title: 'Legacy AI',
+        body: `
+          <p>
+            Our artificial intelligence evaluates the collected information about the user and utilizes it to deliver individual and effective solutions. Over time, the AI turns into a personal coach which knows more about the user, than he knows about himself.
+          </p>
+          <br />
+          <p>
+            It will be able to find errors in a person’s behavior, thinking and habit which lead to failure in certain important life areas. By systematically adjusting these things, the user will experience a great improvement in one’s life quality.
+          </p>
+        `,
+        img: {
+          src: '/mockups/Legacy/2.webp',
+          alt: 'AI Illustration'
+        }
       },
     ],
   },
