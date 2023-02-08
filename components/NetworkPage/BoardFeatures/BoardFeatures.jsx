9bd9d05afdabb9527fb3c3fa8875caebd3953928
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-02-08 18:00:57 +0200

    updated BoardFeatures

--- components/NetworkPage/BoardFeatures/BoardFeatures.jsx
+++ components/NetworkPage/BoardFeatures/BoardFeatures.jsx
@@ -3,14 +3,22 @@ import useStore from '../../../store/store';
 import content from './content';
 import { useRouter } from 'next/router';
 import AlternatingGrid from '../../Generic/AlternatingGrid/AlternatingGrid';
-import { Button } from '@mui/material';
+import { Button, Tab, Tabs } from '@mui/material';
 import List from '../../Generic/List/List';
 import ListItem from '../../Generic/ListItem/ListItem';
+import { a11yProps } from '../../../utils';
+import TabPanel from '../../Generic/TabPanel/TabPanel';
+import { useState } from 'react';
 
 function BoardFeatures() {
   const { isDarkMode } = useStore((state) => state);
   const { locale } = useRouter();
   const { uuid } = require('crypto')
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <section
@@ -24,20 +32,43 @@ function BoardFeatures() {
     >
       <h2>{content[locale].title}</h2>
       <p>{content[locale].description}</p>
-      <div className={styles.grid}>
-        {content[locale].grid_items.map((item) => (
-          <div key={uuid} className={styles['item']}>
-            <span>{item.top_text}</span>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-            <List className={styles.list}>
-              {item.features.map((feature) => (
-                <ListItem key={uuid} className={styles['list-item']}>{feature}</ListItem>
-              ))}
-            </List>
-          </div>
-        ))}
-      </div>
+
+      <Tabs value={value} className={styles['tabs-wrapper']} onChange={handleChange} aria-label="basic tabs example">
+        <Tab label="Community Board" className={styles.tab} {...a11yProps(0)} />
+        <Tab label="Partners Board" className={styles.tab} {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <div className={styles.grid}>
+          {content[locale].community_items.map((item) => (
+            <div key={uuid} className={styles['item']}>
+              <span>{item.top_text}</span>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+              <List className={styles.list}>
+                {item.features.map((feature) => (
+                  <ListItem key={uuid} className={styles['list-item']}>{feature}</ListItem>
+                ))}
+              </List>
+            </div>
+          ))}
+        </div>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <div className={styles.grid}>
+          {content[locale].partners_items.map((item) => (
+            <div key={uuid} className={styles['item']}>
+              <span>{item.top_text}</span>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+              <List className={styles.list}>
+                {item.features.map((feature) => (
+                  <ListItem key={uuid} className={styles['list-item']}>{feature}</ListItem>
+                ))}
+              </List>
+            </div>
+          ))}
+        </div>
+      </TabPanel>
     </section>
   );
 }
--- components/NetworkPage/BoardFeatures/content.js
+++ components/NetworkPage/BoardFeatures/content.js
@@ -5,7 +5,7 @@ const content = {
   en: {
       title: 'Board Features',
       description: 'Our Community and partners board work Together seamlessly to give you the best of the ecosystem. start by connecting your wallet',
-      grid_items: [
+      community_items: [
         {
           title: 'Onboarding',
           top_text: 'Connect wallet',
@@ -36,6 +36,8 @@ const content = {
           ],
           button: null,
         },
+      ],
+      partners_items: [
         {
           top_text: 'Testing',
           title: 'Bug/Feature reporting',
@@ -69,14 +71,13 @@ const content = {
       ]
     },
   de: {
-      title: 'Network Funktionen',
-      description: 'Unsere Community und unsere Partner arbeiten zusammen, um Ihnen das Beste aus dem Ökosystem zu bieten. Beginnen Sie mit der Verbindung Ihrer Wallet',
-      grid_items: [
+      title: 'Board Features',
+      description: 'Our Community and partners board work Together seamlessly to give you the best of the ecosystem. start by connecting your wallet',
+      community_items: [
         {
-          picture: '/mockups/network-features1.png',
-          title: 'Einarbeitung',
+          title: 'Onboarding',
           top_text: 'Connect wallet',
-          description: 'Jedes Board arbeitet zusammen, um Ihre Erfahrung zu unterstützen. Beginnen Sie hier. ',
+          description: 'Each board work Together seamlessly to aid your experience. start here. ',
           features: [
             'Partners Board',
             'Community Board'
@@ -84,10 +85,9 @@ const content = {
           button: 'Join community',
         },
         {
-          picture: '/mockups/network-features2.png',
-          title: 'Abonnieren Sie die neuesten Updates',
+          title: 'Subscribe for latest updates',
           top_text: 'Events',
-          description: 'Abonnieren Sie unseren Newsletter, um die neuesten Informationen über die Community zu erhalten.',
+          description: 'Subscribe to our newsletter to get the latest community updates',
           features: [
             'Live Events',
             'Virtual Events'
@@ -95,12 +95,43 @@ const content = {
           button: null,
         },
         {
-          picture: '/mockups/network-features3.png',
-          title: 'Leistungsübersicht',
-          top_text: 'Review',
-          description: 'Unsere Produkte arbeiten zusammen und bilden die Talent Science. Beginnen Sie mit einem oder mehreren.',
+          top_text: 'Governance',
+          title: 'Legacy Network DAO',
+          description: 'Subscribe to our newsletter to get the latest community updates',
           features: [
-            'Track Performance',
+            'Live Events',
+            'Virtual Events'
+          ],
+          button: null,
+        },
+      ],
+      partners_items: [
+        {
+          top_text: 'Testing',
+          title: 'Bug/Feature reporting',
+          description: 'Subscribe to our newsletter to get the latest community updates',
+          features: [
+            'Live Events',
+            'Virtual Events'
+          ],
+          button: null,
+        },
+        {
+          top_text: 'Giveaway',
+          title: 'Participate in our giveaway',
+          description: 'Subscribe to our newsletter to get the latest community updates',
+          features: [
+            'Live Events',
+            'Virtual Events'
+          ],
+          button: null,
+        },
+        {
+          top_text: 'Giveaway',
+          title: 'Participate in our giveaway',
+          description: 'Subscribe to our newsletter to get the latest community updates',
+          features: [
+            'Live Events',
             'Virtual Events'
           ],
           button: null,
