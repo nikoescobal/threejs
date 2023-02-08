1df80848956b77340c3a703ca980bbe9c5efb9da
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-02-08 17:33:47 +0200

    updated content

--- components/ProductsNFTPage/Tribes/content.js
+++ components/ProductsNFTPage/Tribes/content.js
@@ -12,13 +12,18 @@ const content = {
         `Ancient Greek heroes were brave, persevering, and able to adapt to any intense situation. In addition, Greek philosophers had a great influence on today's society, science, and politics. Valuing logic-based knowledge, rational observation, and reason and inquiry.`,
         'Vikings are Norse warriors best known for their relentless warfare. What many do not know is that they were also peaceful traders, craftsmen, and farmers. Vikings embodied strength and an appreciation for life like no other, and they believed in the teachings of Norse mythology.',
       ]
-      // description_2: 'Each trait such as animation,  weapon, tribe etc. also has multiple tiers of rarity, resulting in unique and epic combinations for the user to own!',
   },
   de: {
       title: 'The Legacy Tribes',
       description_1: 'The Legacy Network NFT is an in-game character with a unique set of features. There are 5 different tribes that hold The Legacy. They come in different forms of animation, face props, armour, colour, and character traits.',
-      // description_2: 'Each trait such as animation,  weapon, tribe etc. also has multiple tiers of rarity, resulting in unique and epic combinations for the user to own!',
-  }
+      information: [
+        'Ancient African tribes were known for their many diverse cultures, customs, and languages. They developed various forms of art, architecture, and spiritual practices that have been passed down from generation to generation. Additionally, ancient African tribes had complex relationships with their environment and relied heavily on subsistence farming and herding for sustenance.',
+        'They were renowned for their sophisticated culture and their remarkable achievements in architecture, art, and astronomy. They also had a complex social structure and a powerful military. The Aztecs were known for their religious rituals, including human sacrifice and the worship of multiple gods.',
+        'The ancient Egyptians were a deeply spiritual people, and their beliefs were a major part of their culture. Egyptians believed that their gods and goddesses controlled the world and that they could intervene in everyday life. They believed that the gods were responsible for the natural processes of the world.',
+        `Ancient Greek heroes were brave, persevering, and able to adapt to any intense situation. In addition, Greek philosophers had a great influence on today's society, science, and politics. Valuing logic-based knowledge, rational observation, and reason and inquiry.`,
+        'Vikings are Norse warriors best known for their relentless warfare. What many do not know is that they were also peaceful traders, craftsmen, and farmers. Vikings embodied strength and an appreciation for life like no other, and they believed in the teachings of Norse mythology.',
+      ]
+  },
 };
 
 export default content;
