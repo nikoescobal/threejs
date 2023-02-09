2379285ba59bc463132d52c4285a30f54e5392a1
Nikolas Escobal <niko.escobal@gmail.com>
2023-02-09 22:03:45 +0800

    Updated banner

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -360,13 +360,12 @@ function Navbar() {
           data-visible={showBanner}
         >
           Join the Legacy Network giveaway | $1 million in prizes and seed
-          funding |{' '}
-          <Link href="https://legacy-token-giveaway.kickoffpages.com/">
-            Join now, we are waiting for you
-          </Link>
-          <Link href="https://legacy-token-giveaway.kickoffpages.com/">
-            <EastIcon />
-          </Link>
+          funding |
+          <a href="https://legacy-token-giveaway.kickoffpages.com/">
+            Join now, we are waiting for you <EastIcon />
+          </a>
+          {/* <Link href="https://legacy-token-giveaway.kickoffpages.com/">
+          </Link> */}
           <IconButton
             className={styles.close}
             onClick={() => setShowBanner(false)}
