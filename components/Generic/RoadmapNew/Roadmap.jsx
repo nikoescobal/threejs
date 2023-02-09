a26b12d8cf61222a0d97193419c7f8b459097d04
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-02-09 05:14:10 +0200

    refactored Roadmap component

--- components/Generic/RoadmapNew/Roadmap.jsx
+++ components/Generic/RoadmapNew/Roadmap.jsx
@@ -1,99 +1,105 @@
 import { useState } from 'react';
-import { Button } from '@mui/material';
+import { Button, Tab, Tabs, useMediaQuery } from '@mui/material';
 import { IconButton } from '@mui/material';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import RoadmapContent from './RoadmapContent';
 import content from './content';
 import styles from './roadmap.module.scss';
 import useStore from '../../../store/store';
+import { useRouter } from 'next/router';
+import { a11yProps } from '../../../utils';
+import TabPanel from '../TabPanel/TabPanel';
+import CheckIcon from '@mui/icons-material/Check';
 
 export default function RoadmapNew() {
-  const [currentSlideindex, setCurrentSlideindex] = useState(0);
-  const [currentProduct, setCurrentProduct] = useState('legacyApp');
-  const [selectedButton, setSelectedButton] = useState(0);
-
-  const handlePreviousClick = () => {
-    setCurrentSlideindex(currentSlideindex - 1);
-  };
-
-  const handleNextClick = () => {
-    setCurrentSlideindex(currentSlideindex + 1);
-  };
-
-  const handleAppBtnClick = (product, selectedButton) => {
-    console.log(product);
-    setCurrentSlideindex(0);
-    setCurrentProduct(product);
-    setSelectedButton(selectedButton);
-  };
-
-  const currentSlide = content[currentProduct][currentSlideindex];
-  const totalSlides = content[currentProduct].length;
+  const [slideIndex, setSlideIndex] = useState(0);
   const { isDarkMode } = useStore();
+  const { locale } = useRouter();
+  const { uuid } = require('crypto');
+  const [value, setValue] = useState(0);
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
+  const visiblePanels = isDesktop ? 2 : 1;
+  const pages = Math.ceil(content.en.tab_panels.Legacy.length / visiblePanels)
 
-  // onClick={() => handleAppBtnClick("legacyApp")}
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleRightClick = () => {
+    if (slideIndex < pages - 1) {
+      setSlideIndex(slideIndex + 1)
+    }
+  }
+  
+  const handleLeftClick = () => {
+    if (slideIndex > 0) {
+      setSlideIndex(slideIndex - 1)
+    }
+  }
+  
   return (
     <section
-      className={`${styles['roadmap-wrapper']} ${
-        !isDarkMode ? `${styles['light']}` : ''
-      } spacing-x`}
+      className={`
+        ${styles['wrapper']}
+        ${!isDarkMode ? `${styles['light']}` : ''}
+        spacing-x
+        max-1920
+        `}
     >
-      <div className={styles['roadmap-title-wrapper']}>
-        <h3 className={styles['title']}>Our Roadmap</h3>
-        <div className={styles['legacy-button-wrapper']}>
-          <Button
-            className={`${styles['roadmap-button']} ${
-              selectedButton === 0 ? `${styles['active-button']} ` : ''
-            }`}
-            onClick={() => handleAppBtnClick('legacyApp', 0)}
-          >
-            Legacy App
-          </Button>
-          <Button
-            className={`${styles['roadmap-button']} ${
-              selectedButton === 1 ? `${styles['active-button']} ` : ''
-            }`}
-            onClick={() => handleAppBtnClick('legacyWallet', 1)}
-          >
-            Legacy Wallet
-          </Button>
-          <Button
-            className={`${styles['roadmap-button']} ${
-              selectedButton === 2 ? `${styles['active-button']} ` : ''
-            }`}
-            onClick={() => handleAppBtnClick('legacyHealth', 2)}
-          >
-            Legacy Health
-          </Button>
-          <Button
-            className={`${styles['roadmap-button']} ${
-              selectedButton === 3 ? `${styles['active-button']} ` : ''
-            }`}
-            onClick={() => handleAppBtnClick('legacyNFT', 3)}
-          >
-            Legacy NFT
-          </Button>
+      <div className={styles['heading']}>
+        <h2 className={styles['title']}>Our Roadmap</h2>
+        {/* <Tabs value={value} className={styles['tabs-wrapper']} onChange={handleChange}>
+          {content.en.tabs.map((tab) => (
+            <Tab key={uuid} label={tab} {...a11yProps(0)} />
+          ))}
+        </Tabs> */}
+        <div className={styles['tabs-wrapper']}>
+          {content.en.tabs.map((tab) => (
+            <Button className='button-blue' key={uuid}>{tab}</Button>
+          ))}
         </div>
       </div>
 
-      <RoadmapContent slide={currentSlide} />
-      <div className={styles['navigate-wrapper']}>
-        <IconButton
-          disabled={currentSlideindex === 0 ? `${styles.disabled}` : ''}
-          onClick={handlePreviousClick}
-          aria-label="delete"
-        >
+      <div className={styles.main}>
+        {
+          // content.en.tab_panels[content.en.tabs[value]].map((tabPanel, index) => (
+          content.en.tab_panels.Legacy.map((tabPanel, index) => (
+            // <TabPanel key={uuid} value={value} index={index}>
+            <div key={uuid} className={styles.panel} style={{translate: `${slideIndex * visiblePanels * -100}% 0`}}>
+              <div className={styles['panel-headers']}>
+                <span>{tabPanel.phase}</span>
+                <span>{tabPanel.date}</span>
+              </div>
+              <CheckIcon
+                className={`${styles['panel-checkmark']} ${tabPanel.content.includes((el) => el.isChecked) ? styles.complete : ''}`}
+                data-complete={tabPanel.content.some((el) => el.isChecked)}
+              />
+              <div>
+                <ul>
+                  {
+                    tabPanel.content.map((listItem) => (
+                      <li key={uuid} data-complete={listItem.isChecked}>
+                        <CheckIcon />
+                        {listItem.content}
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+
+            </div>
+            // </TabPanel>
+          ))
+        }
+      </div>
+      <div className={styles['arrows-wrapper']}>
+        <IconButton onClick={handleLeftClick}>
           <NavigateBeforeIcon />
         </IconButton>
-        {`${currentSlideindex + 1} / ${totalSlides}`}
-        <IconButton
-          disabled={
-            currentSlideindex === totalSlides - 1 ? `${styles.disabled}` : ''
-          }
-          onClick={handleNextClick}
-          aria-label="delete"
-        >
+          {slideIndex + 1}
+          /
+          {pages}
+        <IconButton onClick={handleRightClick}>
           <NavigateNextIcon />
         </IconButton>
       </div>
