74311e8e1e2b4f7e00422dc7ab44a73e8d0969df
El-Potato-Slayer <leighchad.cl@gmail.com>
2023-02-09 09:14:51 +0200

    made TeamSlider Tablet and Mobile responsive

--- components/AboutUs/TeamSlider/TeamSlider.jsx
+++ components/AboutUs/TeamSlider/TeamSlider.jsx
@@ -2,7 +2,7 @@ import styles from './TeamSlider.module.scss';
 import TeamCard from '../TeamCard/TeamCard';
 import EastIcon from '@mui/icons-material/East';
 import WestIcon from '@mui/icons-material/West';
-import { IconButton } from '@mui/material';
+import { IconButton, useMediaQuery } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 import useStore from '../../../store/store';
 
@@ -10,26 +10,28 @@ function TeamSlider({team, offset}) {
   const containerRef = useRef(null);
   const { isDarkMode } = useStore();
   const [x, setX] = useState(0);
-  const [index, setIndex] = useState(0);
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
+  const [slideIndex, setIndex] = useState(0);
   /* length of list times scroll offset * 0.5 (half of the photos occupy the screen initially) */
   const maxRight = team.length * offset * -0.5
   /* Amount of cards visible at once */
-  const visibleCards = 4;
+  const visibleCards = isDesktop ? 4 : 2;
+  // const visibleCards = 4;
   const { uuid } = require('crypto')
-  /* length - visible cards amount + current index - remainder */
-  // const endOffset = team.length - visibleCards + index - (team.length % visibleCards)
+  /* length - visible cards amount + current slideIndex - remainder */
+  // const endOffset = team.length - visibleCards + slideIndex - (team.length % visibleCards)
   
   const handleRightClick = () => {
-    if (index < team.length - visibleCards) {
+    if (slideIndex < team.length - visibleCards) {
       setX(x - 100)
-      setIndex(index + 1)
+      setIndex(slideIndex + 1)
     }
   }
 
   const handleLeftClick = () => {
-    if (x < 0) {
+    if (slideIndex > 0) {
       setX(x + 100)
-      setIndex(index - 1)
+      setIndex(slideIndex - 1)
     }
   }
 
@@ -38,21 +40,32 @@ function TeamSlider({team, offset}) {
     for (let i = visibleCards; i < containerRef.current.children.length ; i++) {
       containerRef.current.children[i].style.opacity = 0
     }
-  }, [])
+    for (let i = 0; i < visibleCards ; i++) {
+      containerRef.current.children[i].style.opacity = 1
+    }
+  }, [visibleCards])
 
   useEffect(() => {
       const length = containerRef.current.children.length 
-      containerRef.current.children[index].style.opacity = '1'
-      if (index > 0) {
-        containerRef.current.children[index - 1].style.opacity = '0'
+      containerRef.current.children[slideIndex].style.opacity = '1'
+      if (slideIndex > 0) {
+        containerRef.current.children[slideIndex - 1].style.opacity = '0'
       }
-      /* Divide cards into length / visibleCards amount of sections and move through them with index */
-      const endOffset = Math.ceil(team.length / visibleCards) + 1 + index
-      if (length !== endOffset ) {
-        containerRef.current.children[endOffset].style.opacity = '0'
+      if (isDesktop) {
+        
+        /* Divide cards into length / visibleCards amount of sections and move through them with slideIndex */
+        const endOffset = Math.ceil(team.length / visibleCards) + 1 + slideIndex
+        if (length !== endOffset ) {
+          containerRef.current.children[endOffset].style.opacity = '0'
+        }
+        containerRef.current.children[endOffset - 1].style.opacity = '1'
+      } else {
+        if (slideIndex + 1) {
+          containerRef.current.children[slideIndex + 1].style.opacity = '1'
+        }
+
       }
-      containerRef.current.children[endOffset - 1].style.opacity = '1'
-  }, [index])
+    }, [slideIndex])
   
   return (
     <div className={`
@@ -78,7 +91,7 @@ function TeamSlider({team, offset}) {
               member={member}
               index={index}
               style={{
-                translate: `${x}% 0`
+                translate: `${slideIndex * -100}% 0`
                 // transform: `translateX(${x}%)`
               }}
             />
