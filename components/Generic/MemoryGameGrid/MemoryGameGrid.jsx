9ae2f76550e6f04694411fce024ba61bc5fe3e6d
Nikolas Escobal <niko.escobal@gmail.com>
2023-02-17 01:39:08 +0800

    Reworked start and restart buttons

--- components/Generic/MemoryGameGrid/MemoryGameGrid.jsx
+++ components/Generic/MemoryGameGrid/MemoryGameGrid.jsx
@@ -90,19 +90,13 @@ function MemoryGameGrid(props) {
       `}
     >
       {!gameStart && !gameRestart ? (
-        <Button
-          className={`${styles['memory-button']} button-blue`}
-          onClick={handleStart}
-        >
+        <Button className={styles['memory-button']} onClick={handleStart}>
           <img src={Start.src} alt="Start icon" className={styles.start} />
           Start
         </Button>
       ) : null}
       {!gameStart && gameRestart ? (
-        <Button
-          className={`${styles['memory-button']} button-blue`}
-          onClick={handleRestart}
-        >
+        <Button className={styles['memory-button']} onClick={handleRestart}>
           <img
             src={Restart.src}
             alt="Restart icon"
